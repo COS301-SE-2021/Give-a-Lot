e8702b2ce8b676aa0e499f72e3720d247a269684
Joshua <u18227369@tuks.co.za>
2021-06-01 10:15:22 +0200

    Updated Certificate Unit Test

--- backend/src/test/java/cs/givealot/CertificateUnitTest/CertificateTest.java
+++ backend/src/test/java/cs/givealot/CertificateUnitTest/CertificateTest.java
@@ -1,4 +1,31 @@
 package test.java.cs.givealot.CertificateUnitTest;
 
 public class CertificateTest {
+
+    String nameOfOrganisation;
+    String descriptionOFOrganisation;
+    String Email;
+    String Address;
+    String Url;
+    Boolean isVerified;
+    Boolean addressIsValid;
+
+    public CertificateTest(String nameOfOrganisation, String descriptionOFOrganisation, String Email, String Address, String Url){
+        this.nameOfOrganisation = nameOfOrganisation;
+        this.descriptionOFOrganisation = descriptionOFOrganisation;
+        this.Email = Email;
+        this.Address = Address;
+        this.Url = Url;
+        this.isVerified = false;
+        this.addressIsValid = false;
+    }
+
+    public String getNameOfOrganisation() {
+        return nameOfOrganisation;
+    }
+
+    public String getDescriptionOFOrganisation() {
+        return descriptionOFOrganisation;
+    }
+    p
 }

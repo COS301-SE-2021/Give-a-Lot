97ddf8e04f9a5a534c9aa2ab9981f2da68c1b92c
TshilidziN <64367951+TshilidziN@users.noreply.github.com>
2021-06-01 22:48:39 +0200

    Updated the login file

--- frontend/givealot/src/App.js
+++ frontend/givealot/src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Login from './components/login/Login'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+      <Login />
     </div>
   );
 }

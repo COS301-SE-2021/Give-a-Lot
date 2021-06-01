b333ca917d29d6d3fbb9be2692df2fe7b957ede0
Joshua <u18227369@tuks.co.za>
2021-06-01 11:50:46 +0200

    Updated Certificate Unit Test - Added functions

---
+++ backend/src/test/java/cs/givealot/CertificateUnitTest/CertificateTest.java
@@ -0,0 +1,12 @@
+package test.java.cs.givealot.CertificateUnitTest;
+
+public class CertificateTest {
+
+    //set up server
+
+    //create pdf
+
+    //upload pdf to server
+
+    //create json nft
+}

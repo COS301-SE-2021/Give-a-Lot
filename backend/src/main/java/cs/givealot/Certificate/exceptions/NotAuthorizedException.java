5f4863148afc61d23fd983b72da1553aa896131b
Joshua <u18227369@tuks.co.za>
2021-06-01 20:03:14 +0200

    setting up exceptions

--- backend/src/main/java/cs/givealot/Certificate/exceptions/NotAuthorizedException.java
+++ backend/src/main/java/cs/givealot/Certificate/exceptions/NotAuthorizedException.java
@@ -1,4 +1,10 @@
 package main.java.cs.givealot.Certificate.exceptions;
 
-public class NotAuthorizedException {
+public class NotAuthorizedException extends CertificateException {
+    public NotAuthorizedException(){}
+    public NotAuthorizedException(String message){super(message);}
+    public NotAuthorizedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
 }

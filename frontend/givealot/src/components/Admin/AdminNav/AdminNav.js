f1b560f68f0ad2400d3cb497e5a84abceb4065d8
WandaWeira <55826359+WandaWeira@users.noreply.github.com>
2021-06-02 16:31:36 +0200

    Admin
    Interface

---
+++ frontend/givealot/src/components/Admin/AdminNav/AdminNav.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import './App.css';
+
+export default class AdminNav extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}

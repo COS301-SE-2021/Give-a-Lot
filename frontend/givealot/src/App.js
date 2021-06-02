285c916149fccc23c3d867ab4a6525b7469a8d05
WandaWeira <55826359+WandaWeira@users.noreply.github.com>
2021-06-02 14:44:26 +0200

    GeneralUser
    Interface Created

--- frontend/givealot/src/App.js
+++ frontend/givealot/src/App.js
@@ -4,42 +4,44 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Registration from "./components/registration/registration"; 
 import Login from "./components/login/login";
+import GeneralUser from "./components/GeneralUser/GeneralUser";
 
 function App() {
-  // return (
+  
+  return (
+  // <Router>
   //   <div className="App">
-  //    <Registration />
+  //     <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+  //       <div className="container">
+  //         <Link className="navbar-brand" to={"/login"}>Give Alot</Link>
+  //         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+  //           <ul className="navbar-nav ml-auto">
+  //             <li className="nav-item">
+  //               <Link className="nav-link" to={"/login"}>Login</Link>
+  //             </li>
+  //             <li className="nav-item">
+  //               <Link className="nav-link" to={"/Registration"}>Registration</Link>
+  //             </li>
+  //           </ul>
+  //         </div>
+  //       </div>
+  //     </nav>
+
+  //     <div className="outer">
+  //       <div className="inner">
+  //         <Switch>
+  //           <Route exact path='/' component={Login} />
+  //           <Route path="/login" component={Login} />
+  //           <Route path="/Registration" component={Registration} />
+  //         </Switch>
+  //       </div>
+  //     </div>
   //   </div>
-  // );
+  // </Router>
 
-  return (<Router>
-    <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-        <div className="container">
-          <Link className="navbar-brand" to={"/login"}>Give Alot</Link>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to={"/login"}>Login</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/Registration"}>Registration</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-      <div className="outer">
-        <div className="inner">
-          <Switch>
-            <Route exact path='/' component={Login} />
-            <Route path="/login" component={Login} />
-            <Route path="/Registration" component={Registration} />
-          </Switch>
-        </div>
-      </div>
-    </div></Router>
+  <div className="App">
+    <GeneralUser/>
+  </div>
   );
 }
 
--- frontend/givealot/src/components/generalUser/generalUser.js
+++ frontend/givealot/src/components/generalUser/generalUser.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
+import Navbar from "../../components/GeneralUser/Navbar/Navbar";
+
 export default class GeneralUser extends Component {
     render() {
         return (
             <div className="">
-                
+                <Navbar />
+                <h1 style={{position: "absolute", left: "450px", top: "300px"}}>
+                    here are the stuff
+                </h1>
             </div>
         );
     }

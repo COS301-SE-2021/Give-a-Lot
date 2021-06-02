93d9fbd02ec780012ac5757e1619fa8131475f36
Joshua <u18227369@tuks.co.za>
2021-06-02 19:14:21 +0200

    Adding Certification files

---
+++ backend/src/main/java/com/GiveaLot/givealot/Certificate/exceptions/JSONException.java
@@ -0,0 +1,7 @@
+package com.GiveaLot.givealot.Certificate.exceptions;
+
+public class JSONException extends Exception{
+    public JSONException(){}
+    public JSONException(String message){ super(message);}
+    public JSONException(String message,Throwable cause){super(message,cause);}
+}

215376cf1597fb43556d6a738f585e2e0a1bf52e
Joshua <u18227369@tuks.co.za>
2021-06-02 21:03:25 +0200

    certificate

--- backend/src/test/java/com/GiveaLot/givealot/CertificateUnitTest/JSONTest.java
+++ backend/src/test/java/com/GiveaLot/givealot/CertificateUnitTest/JSONTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
+
 public class JSONTest {
     public static void main(String[] args) throws Exception {
 

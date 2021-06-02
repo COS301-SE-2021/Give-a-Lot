0dcf55716c0408cc6ef1b92686c604026c9a43f0
Joshua <u18227369@tuks.co.za>
2021-06-02 21:05:27 +0200

    added mockito dependencies

---
+++ backend/src/test/java/com/GiveaLot/givealot/CertificateUnitTest/JSONUnitTest.java
@@ -0,0 +1,9 @@
+package com.GiveaLot.givealot.CertificateUnitTest;
+
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class JSONUnitTest {
+}

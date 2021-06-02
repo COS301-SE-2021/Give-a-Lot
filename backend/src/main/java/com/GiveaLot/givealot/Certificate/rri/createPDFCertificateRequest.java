eda4d27ed9f28cf56f27038e2963474aee2ad904
Joshua <u18227369@tuks.co.za>
2021-06-02 19:14:09 +0200

    Adding Certification files

--- backend/src/main/java/com/GiveaLot/givealot/Certificate/rri/createPDFCertificateRequest.java
+++ backend/src/main/java/com/GiveaLot/givealot/Certificate/rri/createPDFCertificateRequest.java
@@ -1,4 +1,18 @@
 package com.GiveaLot.givealot.Certificate.rri;
 
 public class createPDFCertificateRequest {
+    public String name;
+    public String description;
+    public String email;
+    public String address;
+    public String url;
+
+    public createPDFCertificateRequest(String name, String description, String email, String address, String url) {
+        this.name = name;
+        this.description = description;
+        this.email = email;
+        this.address = address;
+        this.url = url;
+    }
+
 }

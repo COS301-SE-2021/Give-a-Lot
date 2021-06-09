c6f9d02812a2971dbcab50063a3a350104cea9d5
WandaWeira <55826359+WandaWeira@users.noreply.github.com>
2021-06-09 11:48:27 +0200

    Admin user-table
    Adjusted

--- frontend/givealot/src/components/admin/AdminBodyUser.js
+++ frontend/givealot/src/components/admin/AdminBodyUser.js
@@ -1,90 +1,140 @@
 import React from 'react';
 import "./AdminBodyUser.css";
+import SearchIcon from '@material-ui/icons/Search';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
-import SearchIcon from '@material-ui/icons/Search';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import Button from '@material-ui/core/Button';
 
-const columns = [
-    { id: 'name', label: 'Name', minWidth: 170 },
-    { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
-    {
-      id: 'population',
-      label: 'Population',
-      minWidth: 170,
-      align: 'right',
-      format: (value) => value.toLocaleString('en-US'),
+
+const useRowStyles = makeStyles({
+  root: {
+    '& > *': {
+      borderBottom: 'unset',
     },
-    {
-      id: 'size',
-      label: 'Size\u00a0(km\u00b2)',
-      minWidth: 170,
-      align: 'right',
-      format: (value) => value.toLocaleString('en-US'),
-    },
-    {
-      id: 'density',
-      label: 'Density',
-      minWidth: 170,
-      align: 'right',
-      format: (value) => value.toFixed(2),
-    },
-  ];
-  
-  function createData(name, code, population, size) {
-    const density = population / size;
-    return { name, code, population, size, density };
-  }
-  
-  const rows = [
-    createData('India', 'IN', 1324171354, 3287263),
-    createData('China', 'CN', 1403500365, 9596961),
-    createData('Italy', 'IT', 60483973, 301340),
-    createData('United States', 'US', 327167434, 9833520),
-    createData('Canada', 'CA', 37602103, 9984670),
-    createData('Australia', 'AU', 25475400, 7692024),
-    createData('Germany', 'DE', 83019200, 357578),
-    createData('Ireland', 'IE', 4857000, 70273),
-    createData('Mexico', 'MX', 126577691, 1972550),
-    createData('Japan', 'JP', 126317000, 377973),
-    createData('France', 'FR', 67022000, 640679),
-    createData('United Kingdom', 'GB', 67545757, 242495),
-    createData('Russia', 'RU', 146793744, 17098246),
-    createData('Nigeria', 'NG', 200962417, 923768),
-    createData('Brazil', 'BR', 210147125, 8515767),
-  ];
-  
-  const useStyles = makeStyles({
-    root: {
-      width: '100%',
-    },
-    container: {
-      maxHeight: 440,
-    },
-  });
-  
+  },
+});
+
+function createData(name, email, donation, datejoined, status) {
+  return {
+    name,
+    email,
+    donation,
+    datejoined,
+    status,
+    // history: [
+    //   { date: '2020-01-05', customerId: '11091700', amount: 3 },
+    //   { date: '2020-01-02', customerId: 'Anonymous', amount: 1 },
+    // ],
+  };
+}
+
+function Row(props) {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
+  const classes = useRowStyles();
+
+  return (
+    <React.Fragment>
+      <TableRow className={classes.root}>
+        <TableCell>
+          <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {row.name}
+        </TableCell>
+        <TableCell align="right">{row.email}</TableCell>
+        <TableCell align="right">{row.donation}</TableCell>
+        <TableCell align="right">{row.datejoined}</TableCell>
+        <TableCell align="right">{row.status}</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box margin={1}>
+              <Typography variant="h6" gutterBottom component="div">
+                Manage
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>
+                      <Button variant="contained" color="primary">
+                          Revoke
+                      </Button>
+                    </TableCell>
+                    <TableCell>
+                        <Button variant="contained" color="primary">
+                              View
+                          </Button>
+                    </TableCell>
+                    <TableCell>
+                        <Button variant="contained" color="primary">
+                              Verify
+                          </Button>
+                    </TableCell>
+                    <TableCell>
+                        <Button variant="contained" color="primary">
+                              Generate
+                          </Button>
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
+
+// Row.propTypes = {
+//   row: PropTypes.shape({
+//     calories: PropTypes.number.isRequired,
+//     carbs: PropTypes.number.isRequired,
+//     fat: PropTypes.number.isRequired,
+//     history: PropTypes.arrayOf(
+//       PropTypes.shape({
+//         amount: PropTypes.number.isRequired,
+//         customerId: PropTypes.string.isRequired,
+//         date: PropTypes.string.isRequired,
+//       }),
+//     ).isRequired,
+//     name: PropTypes.string.isRequired,
+//     price: PropTypes.number.isRequired,
+//     protein: PropTypes.number.isRequired,
+//   }).isRequired,
+// };
+
+const rows = [
+  createData('Give A lot', "give@gmail.com", 50, "12-12-12","Active"),
+  createData('Give A lot', "give@gmail.com", 50, "12-12-12","Active"),
+  createData('Give A lot', "give@gmail.com", 50, "12-12-12","Active"),
+  createData('Give A lot', "give@gmail.com", 50, "12-12-12","Active"),
+  createData('Give A lot', "give@gmail.com", 50, "12-12-12","Active"),
+  createData('Give A lot', "give@gmail.com", 50, "12-12-12","Active"),
+  createData('Give A lot', "give@gmail.com", 50, "12-12-12","Active"),
+  createData('Give A lot', "give@gmail.com", 50, "12-12-12","Active"),
+];
+
+
 
 function AdminBodyUser() {
-
-    const classes = useStyles();
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
-
     return (
         <div className="adminbodyUser">
             <div className="headerField">
@@ -93,52 +143,29 @@ function AdminBodyUser() {
                     <SearchIcon/>
                     <input placeholder="search User" type="text" />
                 </div>
-            </div>
-            {/* <h4>Users</h4> */}
-            <Paper className={classes.root}>
-                <TableContainer className={classes.container}>
-                    <Table stickyHeader aria-label="sticky table">
+                <div className="tablecontent">
+                <TableContainer component={Paper}>
+                  <Table aria-label="collapsible table">
                     <TableHead>
-                        <TableRow>
-                        {columns.map((column) => (
-                            <TableCell
-                            key={column.id}
-                            align={column.align}
-                            style={{ minWidth: column.minWidth }}
-                            >
-                            {column.label}
-                            </TableCell>
-                        ))}
-                        </TableRow>
+                      <TableRow>
+                        <TableCell />
+                        <TableCell>Name and Surname</TableCell>
+                        <TableCell align="right">Email</TableCell>
+                        <TableCell align="right">Donation&nbsp;(R)</TableCell>
+                        <TableCell align="right">Date Joined</TableCell>
+                        <TableCell align="right">Status</TableCell>
+                      </TableRow>
                     </TableHead>
                     <TableBody>
-                        {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-                        return (
-                            <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                            {columns.map((column) => {
-                                const value = row[column.id];
-                                return (
-                                <TableCell key={column.id} align={column.align}>
-                                    {column.format && typeof value === 'number' ? column.format(value) : value}
-                                </TableCell>
-                                );
-                            })}
-                            </TableRow>
-                        );
-                        })}
+                      {rows.map((row) => (
+                        <Row key={row.name} row={row} />
+                      ))}
                     </TableBody>
-                    </Table>
+                  </Table>
                 </TableContainer>
-                <TablePagination
-                    rowsPerPageOptions={[10, 25, 100]}
-                    component="div"
-                    count={rows.length}
-                    rowsPerPage={rowsPerPage}
-                    page={page}
-                    onChangePage={handleChangePage}
-                    onChangeRowsPerPage={handleChangeRowsPerPage}
-                />
-            </Paper>
+                </div>
+                
+            </div>
         </div>
     )
 }

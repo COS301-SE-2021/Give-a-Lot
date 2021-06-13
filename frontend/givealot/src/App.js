7baf81d375a051bc34d6125bee0ecb27263d8a61
TshilidziN <64367951+TshilidziN@users.noreply.github.com>
2021-06-13 23:45:08 +0200

    Updated BasicUser file

--- frontend/givealot/src/App.js
+++ frontend/givealot/src/App.js
@@ -1,29 +1,22 @@
 import './App.css';
 //import Admin from "./components/admin/Admin";
 //import BasicUser from "./components/basicUser/BasicUser";
-import Login from "./components/login/Login";
-//import Register from "./components/basic/Register";
+//import Login from "./components/login/Login";
+import Register from "./components/register/Register";
 import BasicUser from "./components/basicUser/BasicUser";
+import Login from './components/login/Login';
 //import Organisation from "./components/organisation/Organisation";
 //import Registration from "./components/registration/Registration";
 //import OrgRegistration from "./components/registration/OrgRegistration";
 
 function App() {
-  const na=1;
-  if(na==0){
-    return (
-      <div className="App">
-        <Login />
-      </div>
-   );
-  }
-  else{
+ 
     return (
       <div className="App">
         <BasicUser />
       </div>
    );
-  }
+  
 }
 
 export default App;

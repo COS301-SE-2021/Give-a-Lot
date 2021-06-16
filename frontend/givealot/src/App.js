c8b269392214af11638acca8b0f6350435299eba
TshilidziN <64367951+TshilidziN@users.noreply.github.com>
2021-06-16 16:54:41 +0200

    Fixed errors

--- frontend/givealot/src/App.js
+++ frontend/givealot/src/App.js
@@ -13,8 +13,8 @@ function App() {
  
     return (
       <div className="App">
-        {/* <BasicUserReport /> */}
-        <OrganisationReport />
+        <BasicUserReport />
+          {/*<OrganisationReport />*/}
       </div>
    );
   
--- frontend/givealot/src/components/basicUser/ReportForm.js
+++ frontend/givealot/src/components/basicUser/ReportForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
@@ -7,6 +7,7 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import {makeStyles} from "@material-ui/core/styles";
 import MenuItem from "@material-ui/core/MenuItem";
+import axios from 'axios';
 
 
 
@@ -23,144 +24,192 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-const currencies = [
+const TypeChoices = [
     {
-        value: 'USD',
+        value: 'one',
         label: 'Incorect Profile Information',
     },
     {
-        value: 'EUR',
+        value: 'two',
         label: 'Suspicious Behavior',
     },
     {
-        value: 'BTC',
+        value: 'three',
         label: 'Fraud Activity',
     },
     {
-        value: 'JPY',
+        value: 'four',
         label: 'Other stuff',
     },
 ];
 
+class ReportForm extends Component {
 
-export default function ReportForm() {
-    const classes = useStyles();
-    return (
-        <React.Fragment>
+    constructor(props) {
+        super(props)
 
-            <Grid container spacing={3}>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        required
-                        id="Name"
-                        name="Name"
-                        label="Organisation name"
-                        fullWidth
-                        autoComplete="given-name"
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        required
-                        id="department"
-                        name="department"
-                        label="Department"
-                        fullWidth
-                        autoComplete="department"
-                    />
-                </Grid>
+        this.state = {
+            Name: "",
+            department : "",
+            time: "",
+            date: "",
+            type: "",
+            description: "",
+            country: "",
+            Follow: ""
+        }
+    }
 
-                <Grid item xs={12}>
+    changeHandler = (e) =>{
+        this.setState({[e.target.name] : e.target.value})
+    }
+    submitHandler = (e) =>{
+        e.preventDefault()
+        console.log(this.state)
+        axios.post('https://jsonplaceholder.typicode.com/posts', this.state)
+            .then(response =>{
+                console.log(response)
+            })
+            .catch(error =>{
+                console.log(error)
+            })
+    }
 
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        id="time"
-                        label="Time"
-                        type="time"
-                        defaultValue="07:30"
-                        fullWidth
+    render() {
+        const {Name, department, time,date, type, description,country,Follow} = this.state
+        // const classes = useStyles();
+        return (
+            <form onSubmit={this.submitHandler}>
+                <React.Fragment >
+                    <Grid container spacing={3}>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                required
+                                id="Name"
+                                name="Name"
+                                label="Organisation name"
+                                fullWidth
+                                autoComplete="given-name"
+                                value={Name} onChange={this.changeHandler}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                required
+                                id="department"
+                                name="department"
+                                label="Department"
+                                fullWidth
+                                autoComplete="department"
+                                value={department} onChange={this.changeHandler}
+                            />
+                        </Grid>
 
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        inputProps={{
-                            step: 300, // 5 min
-                        }}
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        required
-                        id="date"
-                        name="date"
-                        label="Date"
-                        type="date"
-                        fullWidth
-                        defaultValue="2017-05-24"
+                        <Grid item xs={12}>
 
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        id="type"
-                        select
-                        label="Please select report Type"
-                        fullWidth
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                id="time"
+                                label="Time"
+                                type="time"
+                                name="time"
+                                defaultValue="07:30"
+                                fullWidth
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                                inputProps={{
+                                    step: 300, // 5 min
+                                }}
+                                value={time} onChange={this.changeHandler}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                required
+                                id="date"
+                                name="date"
+                                label="Date"
+                                type="date"
+                                fullWidth
+                                defaultValue="2017-05-24"
 
-                    >
-                        {currencies.map((option) => (
-                            <MenuItem key={option.value} value={option.value}>
-                                {option.label}
-                            </MenuItem>
-                        ))}
-                    </TextField>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        required
-                        id="country"
-                        name="country"
-                        label="Country"
-                        fullWidth
-                        autoComplete="shipping country"
-                    />
-                </Grid>
-                <Grid item xs={12} >
-                    <TextField
-                        id="outlined-multiline-static"
-                        label="Description"
-                        multiline
-                        rows={8}
-                        fullWidth
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                                value={date} onChange={this.changeHandler}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                id="type"
+                                select
+                                label="Please select report Type"
+                                fullWidth
+                                name="type"
+                                value={type} onChange={this.changeHandler}
 
-                        variant="outlined"
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
+                            >
+                                {TypeChoices.map((option) => (
+                                    <MenuItem key={option.value} value={option.value}>
+                                        {option.label}
+                                    </MenuItem>
+                                ))}
+                            </TextField>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                required
+                                id="country"
+                                name="country"
+                                label="Country"
+                                fullWidth
+                                autoComplete="shipping country"
+                                value={country} onChange={this.changeHandler}
+                            />
+                        </Grid>
+                        <Grid item xs={12} >
+                            <TextField
+                                id="outlined-multiline-static"
+                                label="Description"
+                                multiline
+                                rows={8}
+                                fullWidth
+                                name="description"
+                                variant="outlined"
+                                value={description} onChange={this.changeHandler}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
 
-                        id="Follow"
-                        name="Follow"
-                        label="Follow Up Recommendations"
-                        fullWidth
-                        autoComplete="Follow "
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <Button
-                        variant="contained"
-                        color="primary"
-                       // onClick={handleNext}
-                        className={classes.button}
-                    >
-                        Report
-                    </Button>
-                </Grid>
-            </Grid>
-        </React.Fragment>
-    );
+                                id="Follow"
+                                name="Follow"
+                                label="Follow Up Recommendations"
+                                fullWidth
+                                autoComplete="Follow "
+                                value={Follow} onChange={this.changeHandler}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                // onClick={handleNext}
+                                // className={classes.button}
+                            >
+                                Report
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </React.Fragment>
+            </form>
+
+        );
+
+    }
+
 }
+
+export default ReportForm

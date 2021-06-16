0b4f3d9c83e1ff2560d57802367e7eaada2793cc
Joshua <u18227369@tuks.co.za>
2021-06-16 11:30:57 +0200

    Creating Report Service class

--- backend/src/main/java/cs/givealot/Report/ReportService.java
+++ backend/src/main/java/cs/givealot/Report/ReportService.java
@@ -1,4 +1,20 @@
 package main.java.cs.givealot.Report;
 
-public class ReportService {
+import main.java.cs.givealot.Report.exceptions.CertificateException;
+import main.java.cs.givealot.Report.exceptions.InvalidRequestException;
+import main.java.cs.givealot.Report.exceptions.NotAuthorizedException;
+
+public interface ReportService {
+
+    /**
+     * Generates a new Report .
+     * @param request:createReportRequest Object
+     * @return createReportResponse object that holds the created report
+     * @throws NotAuthorizedException if user is not an admin that is trying to generate report
+     * @throws CertificateException if Report could not be generated for some reason
+     * @throws InvalidRequestException if the request object is not correct
+     * @throws TypeException if the type provided is not valid
+     */
+
+    createReportReponse
 }

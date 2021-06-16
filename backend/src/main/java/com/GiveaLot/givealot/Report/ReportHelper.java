7bba2c5a2958b93ee8485c98fbdde79e6a34f4d0
Joshua <u18227369@tuks.co.za>
2021-06-16 15:54:18 +0200

    Added functionality to ReportHelper

--- backend/src/main/java/com/GiveaLot/givealot/Report/ReportHelper.java
+++ backend/src/main/java/com/GiveaLot/givealot/Report/ReportHelper.java
@@ -5,6 +5,12 @@ import com.GiveaLot.givealot.Report.exceptions.ReportException;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class ReportHelper {
 
@@ -14,7 +20,8 @@ public class ReportHelper {
     public File createReportFile(Report report) throws Exception{
         try {
 
-            String reportName = "report" + report.getId().toString();
+            String reportName = "report" + report.getId() + report.getReporterUsername();
+            reportName = reportName.replaceAll("[^a-zA-Z0-9]", "");
             File file = new File(reportName + ".txt");
             if (file.createNewFile()) {
             } else {
@@ -24,14 +31,18 @@ public class ReportHelper {
             String date = report.getDate().toString();
             FileWriter writer = new FileWriter(reportName + ".txt");
 
-            writer.write("ID:");
+            writer.write("ID: ");
             writer.write(id);
-            writer.write("Date:");
+            writer.write("\n");
+            writer.write("Date: ");
             writer.write(date);
-            writer.write("User:");
+            writer.write("\n");
+            writer.write("User: ");
             writer.write(report.getReporterUsername());
-            writer.write("Description:");
+            writer.write("\n");
+            writer.write("Description: ");
             writer.write(report.getReportDescription());
+            writer.write("\n");
 
             writer.close();
 
@@ -40,5 +51,26 @@ public class ReportHelper {
         catch (Exception e) {
             throw new ReportException("Exception: Problem creating Report");
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        LocalDateTime date = LocalDateTime.now();
+        DateTimeFormatter dateTime = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String dateStr = date.format(dateTime);
+
+        
+
+        Report report = new Report("They provided incorrect addresses and contact information", "Incorrect Profile Info", "CoolUser57",dateStr, timestamp);
+        ReportHelper help = new ReportHelper();
+        File reportFile = help.createReportFile(report);
+        System.out.println(reportFile);
+        Scanner reader = new Scanner(reportFile);
+        while (reader.hasNextLine()){
+            String data = reader.nextLine();
+            System.out.println(data);
+        }
+        reader.close();
+
     }
 }

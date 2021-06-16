357755153ba907861e53d454201b2e1df1169749
Joshua <u18227369@tuks.co.za>
2021-06-16 12:23:35 +0200

    Readded files

---
+++ backend/src/main/java/com/GiveaLot/givealot/Report/ReportService.java
@@ -0,0 +1,23 @@
+package main.java.com.givealot.Report;
+
+import main.java.cs.givealot.Report.exceptions.ReportException;
+import main.java.cs.givealot.Report.exceptions.InvalidRequestException;
+import main.java.cs.givealot.Report.exceptions.NotAuthorizedException;
+import main.java.cs.givealot.Report.exceptions.TypeException;
+import main.java.cs.givealot.Report.rri.createReportResponse;
+import main.java.cs.givealot.Report.rri.createReportRequest;
+
+public interface ReportService {
+
+    /**
+     * Generates a new Report .
+     * @param request:createReportRequest Object
+     * @return createReportResponse object that holds the created report
+     * @throws NotAuthorizedException if user is not an admin that is trying to generate report
+     * @throws ReportException if Report could not be generated for some reason
+     * @throws InvalidRequestException if the request object is not correct
+     * @throws TypeException if the type provided is not valid
+     */
+
+    createReportResponse createReport(createReportRequest request) throws Exception;
+}

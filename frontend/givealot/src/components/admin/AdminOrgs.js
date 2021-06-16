23d8ba51e474c018ea4eef042ee44825237b3e77
WandaWeira <55826359+WandaWeira@users.noreply.github.com>
2021-06-16 20:15:47 +0200

    Organisation table
    Admin organisation table created

--- frontend/givealot/src/components/admin/AdminOrgs.js
+++ frontend/givealot/src/components/admin/AdminOrgs.js
@@ -1,5 +1,20 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from 'react';
+import { DataGrid } from '@material-ui/data-grid';
+import axios from 'axios';
+
+const columns = [
+    { field: 'id', headerName: 'ID', width: "100px" },
+    { field: 'title', headerName: 'Organisation Name', width: "300px" },
+    { field: 'body', headerName: 'stuff', width: "300px" },
+    
+  ];
+//   const rows = [
+//     { field: 'id', headerName: 'ID', width: 70 },
+//     { field: 'firstName', headerName: 'First name', width: 130 },
+//     { field: 'lastName', headerName: 'Last name', width: 130 },
+    
+//   ];
+  
 
 export default class AdminOrgs extends Component {
     constructor(props) {
@@ -27,15 +42,17 @@ export default class AdminOrgs extends Component {
         const { posts, error} = this.state
         return (
             <div>
-                List of posts
+                <h4>List of Organisations</h4>
                 {
-                    posts.length ?
-                    posts.map(post => <div key={post.id}>
-                        {post.title}
-                        {post.body}
-                    </div>):
-                    null
-                    
+                    // posts.length ?
+                    // posts.map(post => <div key={post.id}>
+                    //     {post.title}
+                    //     {post.body}
+                    // </div>):
+                    // null
+                    <div style={{ height: 500, width: '1000px' }}>
+                        <DataGrid rows={posts} columns={columns} pageSize={10} checkboxSelection />
+                    </div>    
                 }
                 
                 {

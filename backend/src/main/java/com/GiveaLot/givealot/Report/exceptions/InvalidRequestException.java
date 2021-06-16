c123997ed6807a31daff027a68108611db174ad7
Joshua <u18227369@tuks.co.za>
2021-06-16 12:54:40 +0200

    Added exceptions

--- backend/src/main/java/com/GiveaLot/givealot/Report/exceptions/InvalidRequestException.java
+++ backend/src/main/java/com/GiveaLot/givealot/Report/exceptions/InvalidRequestException.java
@@ -1,4 +1,9 @@
 package com.GiveaLot.givealot.Report.exceptions;
 
-public class InvalidRequestException {
+
+public class InvalidRequestException extends ReportException
+{
+    public InvalidRequestException(){}
+    public InvalidRequestException(String message){super(message);}
+    public InvalidRequestException(String message,Throwable cause){super(message,cause);}
 }

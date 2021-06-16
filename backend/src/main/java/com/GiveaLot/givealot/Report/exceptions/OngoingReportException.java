8c122bafc8ac93b7f3d60acb835a2bf5b9215a57
Kgomotso404 <u19104546@tuks.co.za>
2021-06-16 13:42:22 +0200

    Added OngoingReportException

--- backend/src/main/java/com/GiveaLot/givealot/Report/exceptions/OngoingReportException.java
+++ backend/src/main/java/com/GiveaLot/givealot/Report/exceptions/OngoingReportException.java
@@ -3,16 +3,11 @@ package com.GiveaLot.givealot.Report.exceptions;
 public class OngoingReportException extends Exception{
 
     public OngoingReportException(){}
-    /**
-     * Constructs a new exception with the specified detail message.  The
-     * cause is not initialized, and may subsequently be initialized by
-     * a call to {@link #initCause}.
-     *
-     * @param message the detail message. The detail message is saved for
-     *                later retrieval by the {@link #getMessage()} method.
-     */
+
     public OngoingReportException(String message){
         super(message);
     }
-    public OngoingReportException(String message,Throwable cause){super(message,cause);}
+    public OngoingReportException(String message,Throwable cause){
+        super(message,cause);
+    }
 }

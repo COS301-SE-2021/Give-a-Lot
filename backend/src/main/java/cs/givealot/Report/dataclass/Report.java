ddce05becd48b4175849557143eac286aee96fd6
Joshua <u18227369@tuks.co.za>
2021-06-16 11:19:15 +0200

    Creating Report class

--- backend/src/main/java/cs/givealot/Report/dataclass/Report.java
+++ backend/src/main/java/cs/givealot/Report/dataclass/Report.java
@@ -1,4 +1,12 @@
 package main.java.cs.givealot.Report.dataclass;
 
+import java.security.Timestamp;
+import java.time.format.DateTimeFormatter;
+
 public class Report {
+    String reportDescription;
+    String reportType;
+    String reporterUsername;
+    DateTimeFormatter date;
+    Timestamp id;
 }

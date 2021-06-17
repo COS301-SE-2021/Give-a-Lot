0f7f3eea5fd45262cfad52221ec738766371f94d
Joshua <u18227369@tuks.co.za>
2021-06-17 12:53:04 +0200

    Added Organisation getters/setters

--- backend/src/main/java/com/GiveaLot/givealot/Organisation/rri/addOrganisationRequest.java
+++ backend/src/main/java/com/GiveaLot/givealot/Organisation/rri/addOrganisationRequest.java
@@ -1,6 +1,7 @@
 package com.GiveaLot.givealot.Organisation.rri;
 
 import com.GiveaLot.givealot.Organisation.dataclass.Organisation;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class addOrganisationRequest {
     String orgName;
@@ -11,5 +12,84 @@ public class addOrganisationRequest {
     String contactPerson;
     String contactNumber;
 
-    createRequest
+    addOrganisationRequest(
+            @JsonProperty("organisation") String orgName,
+            @JsonProperty("description") String orgDescription,
+            @JsonProperty("sector") String orgSector,
+            @JsonProperty("email") String orgEmail,
+            @JsonProperty("password") String password,
+            @JsonProperty("contact") String contactPerson,
+            @JsonProperty("number") String contactNumber) {
+        this.orgName = orgName;
+        this.orgDescription = orgDescription;
+        this.orgSector = orgSector;
+        this.orgEmail = orgEmail;
+        this.password = password;
+        this.contactPerson = contactPerson;
+        this.contactNumber = contactNumber;
+
+    }
+
+    /**
+     * Getters
+     **/
+
+    public String getContactPerson() {
+        return contactPerson;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public String getOrgEmail() {
+        return orgEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getOrgSector() {
+        return orgSector;
+    }
+
+    public String getOrgDescription() {
+        return orgDescription;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    /**Setters**/
+
+    public void setOrgEmail(String orgEmail) {
+        this.orgEmail = orgEmail;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setOrgSector(String orgSector) {
+        this.orgSector = orgSector;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public void setContactPerson(String contactPerson) {
+        this.contactPerson = contactPerson;
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
+
+    public void setOrgDescription(String orgDescription) {
+        this.orgDescription = orgDescription;
+    }
+
 }

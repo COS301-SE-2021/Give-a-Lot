6a3fda1098fa0d1e61341fe51725a145ebe0f89f
Joshua <u18227369@tuks.co.za>
2021-06-17 21:31:56 +0200

    Implemented organisation services

--- backend/src/main/java/com/GiveaLot/givealot/Organisation/OrganisationServiceImpl.java
+++ backend/src/main/java/com/GiveaLot/givealot/Organisation/OrganisationServiceImpl.java
@@ -1,7 +1,12 @@
 package com.GiveaLot.givealot.Organisation;
 
+import com.GiveaLot.givealot.Organisation.dataclass.Organisation;
+import com.GiveaLot.givealot.Organisation.dataclass.Status;
 import com.GiveaLot.givealot.Organisation.rri.*;
 import com.GiveaLot.givealot.Organisation.exceptions.*;
+
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
 import java.util.*;
 
 
@@ -13,38 +18,82 @@ public class OrganisationServiceImpl {
     }
     Status status;
 
-    addOrganisationResponse addOrganisation(addOrganisationRequest request)throws InvalidRequestException
-    {
+    OrganisationHelper help = new OrganisationHelper();
+
+    addOrganisationResponse addOrganisation(addOrganisationRequest request) throws InvalidRequestException, NoSuchAlgorithmException, SQLException {
         if (request == null){
             throw new InvalidRequestException("Exception: Organisation could not be added because the request object is null");
         }
 
+        Organisation org = new Organisation(request.getOrgName(),request.getOrgDescription(),request.getOrgSector(),request.getOrgEmail(),request.getPassword(),request.getContactPerson(),request.getContactNumber());
+
+        help.addOrg(org);
+
 
         return null;
     }
 
-    reactivateOrganisationResponse reactivateOrganisation(reactivateOrganisationRequest request)throws InvalidRequestException{
+    reactivateOrganisationResponse reactivateOrganisation(reactivateOrganisationRequest request) throws OrgException, NoSuchAlgorithmException, SQLException {
         if (request == null){
             throw new InvalidRequestException("Exception: Organisation could not be updated because the request object is null");
+        }
+        try {
+            Organisation org = new Organisation(request.getOrgEmail(), request.getStatus());
+
+            if (!(org.getStatus() == com.GiveaLot.givealot.Organisation.dataclass.Status.Active)){
+                help.reactivateOrg(org);
+            }
+            else{
+                throw new OrgException("Exception: Organisation is already Active");
+            }
+        }
+        catch (Exception e){
+            throw new OrgException("Exception: Organisation could not be reactivated");
         }
 
 
         return null;
     }
 
-    investigateOrganisationResponse investigateOrganisation(investigateOrganisationRequest request)throws InvalidRequestException{
+    investigateOrganisationResponse investigateOrganisation(investigateOrganisationRequest request) throws OrgException {
         if (request == null){
             throw new InvalidRequestException("Exception: Organisation could not be updated because the request object is null");
+        }
+        try {
+            Organisation org = new Organisation(request.getOrgEmail(), request.getStatus());
+
+            if (!(org.getStatus() == com.GiveaLot.givealot.Organisation.dataclass.Status.UnderInvestigation)){
+                help.investigateOrg(org);
+            }
+            else{
+                throw new OrgException("Exception: Organisation is already UnderInvestigation");
+            }
+        }
+        catch (Exception e){
+            throw new OrgException("Exception: Organisation could not be set to UnderInvestagtion");
         }
 
 
         return null;
     }
 
-    suspendOrganisationResponse suspendOrganisation(suspendOrganisationRequest request) throws InvalidRequestException
+    suspendOrganisationResponse suspendOrganisation(suspendOrganisationRequest request) throws OrgException
     {
         if (request == null){
             throw new InvalidRequestException("Exception: Organisation could not be updated because the request object is null");
+        }
+        try {
+            Organisation org = new Organisation(request.getOrgEmail(), request.getStatus());
+
+            if (!(org.getStatus() == com.GiveaLot.givealot.Organisation.dataclass.Status.Suspended)){
+                help.suspendOrg(org);
+            }
+            else{
+                throw new OrgException("Exception: Organisation is already Suspended");
+            }
+        }
+        catch (Exception e){
+            throw new OrgException("Exception: Organisation could not be suspended");
         }
 
 

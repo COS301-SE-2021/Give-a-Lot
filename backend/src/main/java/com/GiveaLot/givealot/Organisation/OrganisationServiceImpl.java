bb6e62b651ffd1e45c0f055a4107eb631445211c
Kgomotso404 <u19104546@tuks.co.za>
2021-06-17 15:55:48 +0200

    added mail dependencies to pom file

--- backend/src/main/java/com/GiveaLot/givealot/Organisation/OrganisationServiceImpl.java
+++ backend/src/main/java/com/GiveaLot/givealot/Organisation/OrganisationServiceImpl.java
@@ -2,14 +2,23 @@ package com.GiveaLot.givealot.Organisation;
 
 import com.GiveaLot.givealot.Organisation.rri.*;
 import com.GiveaLot.givealot.Organisation.exceptions.*;
+import java.util.*;
+
 
 public class OrganisationServiceImpl {
+    enum Status{
+        Active,
+        UnderInvestigation,
+        Suspended
+    }
+  Status status;
     suspendOrganisationResponse suspendOrganisation(suspendOrganisationRequest suspendOrganisationRequest) throws InvalidRequestException
     {
        if(suspendOrganisationRequest.getOrgEmail() == null)
        {
            throw new InvalidRequestException("Invalid organisation email");
        }
+
 
         return null;
     }

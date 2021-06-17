c1e547388811b35d6779cdfb67488ff8aa73c37b
Joshua <u18227369@tuks.co.za>
2021-06-17 13:09:14 +0200

    Added Organisation functions

--- backend/src/main/java/com/GiveaLot/givealot/Organisation/OrganisationServiceImpl.java
+++ backend/src/main/java/com/GiveaLot/givealot/Organisation/OrganisationServiceImpl.java
@@ -1,4 +1,29 @@
 package com.GiveaLot.givealot.Organisation;
 
-public class OrganisationServiceImpl {
+import com.GiveaLot.givealot.Organisation.exceptions.InvalidRequestException;
+import com.GiveaLot.givealot.Organisation.exceptions.OrgException;
+import com.GiveaLot.givealot.Organisation.rri.addOrganisationRequest;
+import com.GiveaLot.givealot.Organisation.rri.addOrganisationResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrganisationServiceImpl implements OrganisationService{
+
+    @Override
+    public addOrganisationResponse addOrganisation(addOrganisationRequest request) throws Exception
+    {
+        addOrganisationResponse orgRes = null;
+        if (request == null){
+            throw new InvalidRequestException("Exception: Organisation could not be added because request is null");
+        }
+
+        try {
+
+            //Add organisation to database
+
+        }catch (Exception e){
+            throw new OrgException("Exception: Problem adding Organisation to database");
+        }
+        return orgRes;
+    }
 }

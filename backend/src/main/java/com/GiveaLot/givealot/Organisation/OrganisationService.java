efcb4bbf230949817b2616f189d7c5d6eaf94f36
Joshua <u18227369@tuks.co.za>
2021-06-17 13:09:09 +0200

    Added Organisation functions

--- backend/src/main/java/com/GiveaLot/givealot/Organisation/OrganisationService.java
+++ backend/src/main/java/com/GiveaLot/givealot/Organisation/OrganisationService.java
@@ -1,4 +1,18 @@
 package com.GiveaLot.givealot.Organisation;
 
-public class OrganisationService {
+
+import com.GiveaLot.givealot.Organisation.exceptions.OrgException;
+import com.GiveaLot.givealot.Organisation.rri.addOrganisationRequest;
+import com.GiveaLot.givealot.Organisation.rri.addOrganisationResponse;
+
+public interface OrganisationService {
+    /**
+     * Generates a new Report .
+     * @param request:addOrganisationRequest Object
+     * @return addOrganisationResponse object that holds ...
+     * @throws OrgException if Report could not be generated for some reason
+
+     */
+
+    addOrganisationResponse addOrganisation(addOrganisationRequest request) throws Exception;
 }

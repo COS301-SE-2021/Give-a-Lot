2e737b8bd60906dad54746b800fe7f62c4621651
Joshua <u18227369@tuks.co.za>
2021-06-17 12:44:55 +0200

    Added Organisation Classes

--- backend/src/main/java/com/GiveaLot/givealot/Organisation/rri/addOrganisationRequest.java
+++ backend/src/main/java/com/GiveaLot/givealot/Organisation/rri/addOrganisationRequest.java
@@ -1,4 +1,15 @@
 package com.GiveaLot.givealot.Organisation.rri;
 
+import com.GiveaLot.givealot.Organisation.dataclass.Organisation;
+
 public class addOrganisationRequest {
+    String orgName;
+    String orgDescription;
+    String orgSector;
+    String orgEmail;
+    String password;
+    String contactPerson;
+    String contactNumber;
+
+    createRequest
 }

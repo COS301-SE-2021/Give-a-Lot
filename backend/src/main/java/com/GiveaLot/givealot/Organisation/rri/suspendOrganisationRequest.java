c5de1dbf71487ddf1c8db092fc646a2b9794a405
Kgomotso404 <u19104546@tuks.co.za>
2021-06-17 13:40:19 +0200

    implemented suspendOrganisationRequest

--- backend/src/main/java/com/GiveaLot/givealot/Organisation/rri/suspendOrganisationRequest.java
+++ backend/src/main/java/com/GiveaLot/givealot/Organisation/rri/suspendOrganisationRequest.java
@@ -1,4 +1,39 @@
 package com.GiveaLot.givealot.Organisation.rri;
 
+import com.GiveaLot.givealot.Organisation.dataclass.Organisation;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class suspendOrganisationRequest {
+    enum Status{
+        Active,
+        UnderInvestigation,
+        Suspended
+    }
+    Status status;
+    String orgEmail;
+
+    suspendOrganisationRequest(@JsonProperty("status") Status status,
+                               @JsonProperty("email") String orgEmail)
+    {
+        this.status=status;
+        this.orgEmail = orgEmail;
+    }
+
+    //getters and setters
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public String getOrgEmail() {
+        return orgEmail;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setOrgEmail(String orgEmail) {
+        this.orgEmail = orgEmail;
+    }
 }

fed96d8ee94f944af0e08584b5011bb2499ff44b
Kgomotso404 <u19104546@tuks.co.za>
2021-06-17 13:28:36 +0200

    Added suspendOrganisation request and respond objects

--- backend/src/main/java/com/GiveaLot/givealot/Organisation/OrganisationService.java
+++ backend/src/main/java/com/GiveaLot/givealot/Organisation/OrganisationService.java
@@ -1,4 +1,11 @@
 package com.GiveaLot.givealot.Organisation;
+import com.GiveaLot.givealot.Organisation.rri.*;
 
 public interface OrganisationService {
+    /**
+     * This use case returns a response indicating that the organisation has been suspended
+     * @param suspendOrganisationRequest Request to use case
+     * @return Return the status of the organisation
+     */
+    suspendOrganisationResponse suspendOrganisation(suspendOrganisationRequest suspendOrganisationRequest);
 }

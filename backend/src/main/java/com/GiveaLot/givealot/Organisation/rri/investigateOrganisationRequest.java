bffa5f2e9a7520955e796abb06fcc5c014ada5af
Xplodiator <33042059+Xplodiator@users.noreply.github.com>
2021-06-19 23:04:12 +0200

    overloaded a constructor for investigate Organisation request

--- backend/src/main/java/com/GiveaLot/givealot/Organisation/rri/investigateOrganisationRequest.java
+++ backend/src/main/java/com/GiveaLot/givealot/Organisation/rri/investigateOrganisationRequest.java
@@ -8,11 +8,28 @@ public class investigateOrganisationRequest {
     Status status;
     String orgEmail;
 
-    investigateOrganisationRequest(@JsonProperty("status") Status status,
-                               @JsonProperty("email") String orgEmail)
+    String org_id;
+    String admin_id;
+
+    public investigateOrganisationRequest(@JsonProperty("org_id") String org_id, @JsonProperty("admin_id") String admin_id)
+    {
+        this.org_id = org_id;
+        this.admin_id = admin_id;
+    }
+
+    investigateOrganisationRequest(Status status,
+                                String orgEmail)
     {
         this.status=status;
         this.orgEmail = orgEmail;
+    }
+
+    public String getAdmin_id() {
+        return admin_id;
+    }
+
+    public String getOrg_id() {
+        return org_id;
     }
 
     //getters and setters

14f6277189286c5c112ca89b88fc8ff285d85c1e
Xplodiator <33042059+Xplodiator@users.noreply.github.com>
2021-06-20 16:51:43 +0200

    implemented report response Json class

--- backend/src/main/java/com/GiveaLot/givealot/Report/ReportResponseJSON.java
+++ backend/src/main/java/com/GiveaLot/givealot/Report/ReportResponseJSON.java
@@ -2,4 +2,27 @@ package com.GiveaLot.givealot.Report;
 
 public class ReportResponseJSON
 {
+    int code;
+    String status;
+
+    public ReportResponseJSON(int http_code, String status) {
+        this.code = http_code;
+        this.status = status;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }

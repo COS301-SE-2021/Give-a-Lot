2d9dc9af3f699c698e530006782652372e3a9cbb
TshilidziN <64367951+TshilidziN@users.noreply.github.com>
2021-06-20 15:19:33 +0200

    update controller

--- backend/src/main/java/com/GiveaLot/givealot/Browse/controller/BrowseController.java
+++ backend/src/main/java/com/GiveaLot/givealot/Browse/controller/BrowseController.java
@@ -6,10 +6,7 @@ import com.GiveaLot.givealot.Browse.rri.browseResponse;
 import com.GiveaLot.givealot.Browse.rri.browseResponseJSON;
 import com.GiveaLot.givealot.User.dataclass.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,7 +21,7 @@ public class BrowseController
         BrowseServiceImp = browseServiceImp;
     }
 
-    @PostMapping
+    @GetMapping
     public List<browseResponseJSON> search()
     {
         browseRequest browseRequest = null;

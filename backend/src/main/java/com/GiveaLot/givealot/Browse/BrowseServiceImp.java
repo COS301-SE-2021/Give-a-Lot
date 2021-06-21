45230e2bd05d5480b551e76f51c70ab05ca05326
Xplodiator <33042059+Xplodiator@users.noreply.github.com>
2021-06-21 12:31:51 +0200

    added comments

--- backend/src/main/java/com/GiveaLot/givealot/Browse/BrowseServiceImp.java
+++ backend/src/main/java/com/GiveaLot/givealot/Browse/BrowseServiceImp.java
@@ -12,18 +12,31 @@ import java.util.List;
 @Service
 public class BrowseServiceImp implements BrowseService
 {
-    public browseResponse browse(browseRequest request) throws Exception {
+    /*
+    *   @Todo get user preferred organisations by learning the user's usage patterns
+    *   @Todo filter by country, sector, distance from you (near client or further away)
+    * */
+    public browseResponse browse(browseRequest request) throws Exception
+    {
+
+        /*the helper is responsible for the database query logic*/
         BrowseHelper browseHelper = new BrowseHelper();
         browseResponse browseResponse = new browseResponse();
 
         try
         {
+            /*create a response object which will set the JSON object*/
             browseResponse.setOrganisations(browseHelper.getOrganisations());
             return browseResponse;
         }
         catch (Exception e)
         {
+            System.out.println("error getting organisations");/*for the developer*/
             throw new Exception(e.getMessage());
         }
+    }
+
+    public static void main(String[] args) {
+
     }
 }

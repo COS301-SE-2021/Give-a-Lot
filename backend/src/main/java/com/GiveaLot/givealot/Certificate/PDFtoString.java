2974f4779bda9c7379bf1bd478c3a14d29beb89c
Joshua <u18227369@tuks.co.za>
2021-07-07 16:38:18 +0200

    Fixed and updated existing certificate code

---
+++ backend/src/main/java/com/GiveaLot/givealot/Certificate/PDFtoString.java
@@ -0,0 +1,33 @@
+package com.GiveaLot.givealot.Certificate;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripperByArea;
+
+import javax.swing.text.Document;
+import java.io.File;
+import java.io.IOException;
+
+public class PDFtoString {
+    public String PDFtoString() {
+
+        StringBuilder sb = new StringBuilder();
+        try (
+                PDDocument document = PDDocument.load(new File("C:\\generateCertificates\\TheOrganisation.pdf"))) {
+            document.getClass();
+            if (!document.isEncrypted()) {
+                PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+                stripper.setSortByPosition(true);
+                PDFTextStripper tStripper = new PDFTextStripper();
+                String pdfFileInText = tStripper.getText(document);
+                String lines[] = pdfFileInText.split("\\r?\\n");
+                for (String line : lines) {
+                    sb.append(line);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+}

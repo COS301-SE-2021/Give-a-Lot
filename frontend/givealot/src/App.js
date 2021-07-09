3a10e63cc8fb4e5553e3505abad3d9ba4bede276
TshilidziN <64367951+TshilidziN@users.noreply.github.com>
2021-07-09 17:42:39 +0200

    Login page

--- frontend/givealot/src/App.js
+++ frontend/givealot/src/App.js
@@ -1,11 +1,12 @@
 // import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 import Register from "./components/register/Register";
+import Login from "./components/login/Login";
 import './App.css';
 
 function App() {
     return (
       <div className="App">
-        <Register />
+        <Login />
       </div>
    );
   
---
+++ frontend/givealot/src/components/login/Login.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import "./Login.css";
+import {FaGithub } from "react-icons/fa";
+import logo from "./images/logo.png";
+import body from "./images/body.png";
+
+
+class LoginBody extends Component {
+
+    render() {
+        return (
+            <div >
+                <div className="header">
+                    <div className="image">
+                        <img id="ID" src={logo} alt=""/>
+                    </div>
+
+                </div>
+                <div className="container" >
+
+                 <form className="form">
+                     <div className="top">
+                         <h4> Registration | USER</h4>
+                     </div>
+                    <div >
+                        <label></label>
+
+                        <input type="name" className="control" placeholder=" Name" />
+                    </div>
+
+                    <div >
+                        <label></label>
+                        <input type="name" className="control" placeholder="Surname" />
+                    </div>
+
+                    <div >
+                        <label></label>
+                        <input type="email" className="control" placeholder="Email" />
+                    </div>
+
+                    <div >
+                        <label></label>
+                        <input type="password" className="control" placeholder="Password" />
+                    </div>
+                    <div>
+                        <button type="submit" className="button">Sign Up</button>
+                    </div>
+
+                </form>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default LoginBody

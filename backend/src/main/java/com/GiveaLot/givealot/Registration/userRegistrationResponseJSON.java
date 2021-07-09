063d0785a0f5d6636298a6fd25184b40175efa61
Xplodiator <33042059+Xplodiator@users.noreply.github.com>
2021-07-09 21:29:41 +0200

    implemented jsonResponses

--- backend/src/main/java/com/GiveaLot/givealot/Registration/userRegistrationResponseJSON.java
+++ backend/src/main/java/com/GiveaLot/givealot/Registration/userRegistrationResponseJSON.java
@@ -1,4 +1,28 @@
 package com.GiveaLot.givealot.Registration;
 
-public class userRegistrationResponseJSON {
+public class userRegistrationResponseJSON
+{
+    int status;
+    String message;
+
+    public userRegistrationResponseJSON(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }

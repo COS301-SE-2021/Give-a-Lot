cc7a53d3a62df913b98941a380fe002148a57c3a
Xplodiator <33042059+Xplodiator@users.noreply.github.com>
2021-07-09 18:50:01 +0200

    added the browse package

--- backend/src/test/java/com/GiveaLot/givealot/GivealotApplicationTests.java
+++ backend/src/test/java/com/GiveaLot/givealot/GivealotApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GivealotApplicationTests {
-
+class GivealotApplicationTests
+{
 	@Test
-	void contextLoads() {
+	void contextLoads()
+	{
 	}
-
 }

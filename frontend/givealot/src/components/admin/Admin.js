b459b14977778501f6fbceaa8b39fe4e28d286ce
WandaWeira <55826359+WandaWeira@users.noreply.github.com>
2021-07-17 22:56:18 +0200

    Admin sidebar updated

--- frontend/givealot/src/components/admin/Admin.js
+++ frontend/givealot/src/components/admin/Admin.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react'
 import logo from "./imagesRegister/ID2.png"
 import AdminSidebar from "./AdminSidebar"
+import "./Admin.css"
 
 export class Admin extends Component {
 
     render() {
         return (
             <div className="admin">
-                <div className="header">
-                    <div style={{width: "220px",height: "70px"}}>
-                        <img id="ID" src={logo} alt=""/>
+                <div className="Adminheader">
+                    <div style={{width: "100%",height: "70px"}}>
+                        <img id="ID" src={logo} alt="" />
                     </div>
-
                 </div>
                 <div className="mainBody">
-                    <AdminSidebar />
-                    {/*here is other stuff*/}
+                    <div className="Adminsidebar">
+                        <AdminSidebar />
+                    </div>
+                    <div className="body">
+                        body
+                    </div>
+                   
                 </div>
             </div>
         )
--- frontend/givealot/src/components/admin/AdminSidebar.js
+++ frontend/givealot/src/components/admin/AdminSidebar.js
@@ -7,96 +7,98 @@ import PeopleIcon from '@material-ui/icons/People';
 import BarChartIcon from '@material-ui/icons/BarChart';
 import AssignmentIcon from '@material-ui/icons/Assignment';
 import { Link } from 'react-router-dom';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
+import DescriptionIcon from '@material-ui/icons/Description';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import "./Admin.css"
 
 export class Admin extends Component {
 
     render() {
         return (
-            <div className="sidebar">
-                <div className="sidebarRow">
-                    <Link to={'/dashboard'}>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <DashboardIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Dashboard" />
-                        </ListItem>
-                    </Link>
-                    <Link to={'/adminUser'}>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <PeopleIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="View Users" />
-                        </ListItem>
-                    </Link>
+        <div className="sidebar">
+            <Link to={'/dashboard'}>
+                <ListItem button>
+                    <ListItemIcon>
+                        <DashboardIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Dashboard" />
+                </ListItem>
+            </Link>
+            <Link to={'/adminUser'}>
+                <ListItem button>
+                    <ListItemIcon>
+                        <PeopleIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="View Users" />
+                </ListItem>
+            </Link>
 
-                    <Link to={'/adminOrgs'}>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <PeopleIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="View Organisations" />
-                        </ListItem>
-                    </Link>
+            <Link to={'/adminOrgs'}>
+                <ListItem button>
+                    <ListItemIcon>
+                        <PeopleIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="View Organisations" />
+                </ListItem>
+            </Link>
 
-                    <Link to={'/email'}>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <BarChartIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Emails" />
-                        </ListItem>
-                    </Link>
+            <Link to={'/email'}>
+                <ListItem button>
+                    <ListItemIcon>
+                        <MailOutlineIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Emails" />
+                </ListItem>
+            </Link>
 
-                    <Link to={'/calendar'}>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <BarChartIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Calendar" />
-                        </ListItem>
-                    </Link>
+            <Link to={'/calendar'}>
+                <ListItem button>
+                    <ListItemIcon>
+                        <CalendarTodayIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Calendar" />
+                </ListItem>
+            </Link>
 
-                    <Link to={'/todos'}>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <AssignmentIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="TO-DO's" />
-                        </ListItem>
-                    </Link>
+            <Link to={'/todos'}>
+                <ListItem button>
+                    <ListItemIcon>
+                        <AssignmentIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="TO-DO's" />
+                </ListItem>
+            </Link>
 
-                    <Link to={'/documentation'}>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <AssignmentIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Documentation" />
-                        </ListItem>
-                    </Link>
+            <Link to={'/documentation'}>
+                <ListItem button>
+                    <ListItemIcon>
+                        <DescriptionIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Documentation" />
+                </ListItem>
+            </Link>
 
-                    <Link to={'/stats'}>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <AssignmentIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Statistics" />
-                        </ListItem>
-                    </Link>
+            <Link to={'/stats'}>
+                <ListItem button>
+                    <ListItemIcon>
+                        <BarChartIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Statistics" />
+                </ListItem>
+            </Link>
 
-                    <Link to={'/logout'}>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <AssignmentIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Logout" />
-                        </ListItem>
-                    </Link>
+            <Link to={'/logout'}>
+                <ListItem button>
+                    <ListItemIcon>
+                        <AccountCircleIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Logout" />
+                </ListItem>
+            </Link>
 
-                </div>
-            </div>
+        </div>
         )
     }
 }

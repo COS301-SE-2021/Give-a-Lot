b657c5fbad6b653b30eb4139fa527f991b0d2dc3
WandaWeira <55826359+WandaWeira@users.noreply.github.com>
2021-07-17 22:59:40 +0200

    Admin dashboard

---
+++ frontend/givealot/src/components/admin/AdminDashboard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import "./Admin.css"
+
+export class AdminDashboard extends Component {
+
+    render() {
+        return (
+            <div className="adminDashboard">
+
+            </div>
+        )
+    }
+}
+
+export default AdminDashboard

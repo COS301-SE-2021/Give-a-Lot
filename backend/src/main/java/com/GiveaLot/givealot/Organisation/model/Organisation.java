2191c77eba64a82c5eb821a79fca62067d85ba84
Joshua <u18227369@tuks.co.za>
2021-07-30 19:32:24 +0200

    Added implementation of organisationPoints row mapper

--- backend/src/main/java/com/GiveaLot/givealot/Organisation/model/Organisation.java
+++ backend/src/main/java/com/GiveaLot/givealot/Organisation/model/Organisation.java
@@ -2,6 +2,9 @@ package com.GiveaLot.givealot.Organisation.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
 
 public class Organisation {
@@ -130,5 +133,28 @@ public class Organisation {
 
     public void setPassworrd(String password) {
         this.password = password;
+    }
+
+    public static class MD5 {
+        public String getMd5(String input)
+        {
+            try {
+                MessageDigest md = MessageDigest.getInstance("MD5");
+
+                byte[] messageDigest = md.digest(input.getBytes());
+
+                BigInteger no = new BigInteger(1, messageDigest);
+
+                String hashtext = no.toString(16);
+                while (hashtext.length() < 32) {
+                    hashtext = "0" + hashtext;
+                }
+                return hashtext;
+            }
+            catch (NoSuchAlgorithmException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
     }
 }

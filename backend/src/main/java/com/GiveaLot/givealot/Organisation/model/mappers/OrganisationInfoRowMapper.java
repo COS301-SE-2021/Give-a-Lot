80c3cc6f5a8720724ec49ffb1a3916ecdea66bff
Joshua <u18227369@tuks.co.za>
2021-07-30 16:25:53 +0200

    Added implementation of organisationInfo row mapper

--- backend/src/main/java/com/GiveaLot/givealot/Organisation/model/mappers/OrganisationInfoRowMapper.java
+++ backend/src/main/java/com/GiveaLot/givealot/Organisation/model/mappers/OrganisationInfoRowMapper.java
@@ -1,4 +1,31 @@
 package com.GiveaLot.givealot.Organisation.model.mappers;
 
-public class OrganisationInfoRowMapper {
+import com.GiveaLot.givealot.Organisation.model.Organisation;
+import com.GiveaLot.givealot.Organisation.model.OrganisationInfo;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class OrganisationInfoRowMapper implements RowMapper<OrganisationInfo> {
+    @Override
+    public OrganisationInfo mapRow(ResultSet rs, int i) throws SQLException {
+        OrganisationInfo organisationInfo = new OrganisationInfo(
+                rs.getString("orgId"),
+                rs.getString("address"),
+                rs.getInt("numberOfImages"),
+                rs.getInt("numberOfReports"),
+                rs.getString("website"),
+                rs.getString("auditDocument"),
+                rs.getString("taxReference"),
+                rs.getString("auditorDetails"),
+                rs.getString("committeeDetails"),
+                rs.getString("ngoNumber"),
+                rs.getDate("ngoDate"),
+                rs.getString("twitter"),
+                rs.getString("facebook"),
+                rs.getString("instagram"),
+                rs.getDate("establishmentDate"));
+        return organisationInfo;
+    }
 }

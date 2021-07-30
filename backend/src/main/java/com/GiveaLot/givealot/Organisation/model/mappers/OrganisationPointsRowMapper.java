0d76b56ebb3552c1513ab6dfd19501d89e08c50b
Joshua <u18227369@tuks.co.za>
2021-07-30 16:30:29 +0200

    Added implementation of organisationPoints row mapper

--- backend/src/main/java/com/GiveaLot/givealot/Organisation/model/mappers/OrganisationPointsRowMapper.java
+++ backend/src/main/java/com/GiveaLot/givealot/Organisation/model/mappers/OrganisationPointsRowMapper.java
@@ -1,4 +1,30 @@
 package com.GiveaLot.givealot.Organisation.model.mappers;
 
-public class OrganisationPointsRowMapper {
+import com.GiveaLot.givealot.Organisation.model.OrganisationPoints;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class OrganisationPointsRowMapper implements RowMapper<OrganisationPoints> {
+
+    @Override
+    public OrganisationPoints mapRow(ResultSet rs, int i) throws SQLException {
+        OrganisationPoints organisationPoints = new OrganisationPoints(
+                rs.getString("orgId"),
+                rs.getInt("points"),
+                rs.getBoolean(""),
+                rs.getBoolean(""),
+                rs.getBoolean(""),
+                rs.getBoolean(""),
+                rs.getBoolean(""),
+                rs.getBoolean(""),
+                rs.getBoolean(""),
+                rs.getBoolean(""),
+                rs.getString(""),
+                rs.getBoolean(""),
+                rs.getInt(""));
+
+        return organisationPoints;
+    }
 }

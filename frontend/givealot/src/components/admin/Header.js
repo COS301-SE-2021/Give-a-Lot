efae0a44dca3b4a6b311acc59c42d158591e6c2b
WandaWeira <55826359+WandaWeira@users.noreply.github.com>
2021-07-31 22:09:05 +0200

    few changes

--- frontend/givealot/src/components/admin/Header.js
+++ frontend/givealot/src/components/admin/Header.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import "./Admin.css"
-import logo from "./imagesRegister/ID2.png"
-import ListItemIcon from "@material-ui/core/ListItemIcon";
+// import logo from "./imagesRegister/ID2.png"
+// import ListItemIcon from "@material-ui/core/ListItemIcon";
 // import DashboardIcon from "@material-ui/icons/Dashboard";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItem from "@material-ui/core/ListItem";
-import PrintIcon from '@material-ui/icons/Print';
+// import ListItemText from "@material-ui/core/ListItemText";
+// import ListItem from "@material-ui/core/ListItem";
+// import PrintIcon from '@material-ui/icons/Print';
 
 export class Header extends Component {
 

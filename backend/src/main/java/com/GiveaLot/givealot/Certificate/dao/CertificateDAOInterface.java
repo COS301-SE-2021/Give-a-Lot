008c1e96746cbac45ee3370a83bf4eb4a1018e2f
Joshua <u18227369@tuks.co.za>
2021-08-03 23:20:51 +0200

    Restructuring Certificate

--- backend/src/main/java/com/GiveaLot/givealot/Certificate/dao/CertificateDAOInterface.java
+++ backend/src/main/java/com/GiveaLot/givealot/Certificate/dao/CertificateDAOInterface.java
@@ -1,4 +1,22 @@
 package com.GiveaLot.givealot.Certificate.dao;
 
+import com.GiveaLot.givealot.Certificate.model.Certificate;
+import com.itextpdf.text.Document;
+
+import javax.mail.internet.MimeMessage;
+
 public interface CertificateDAOInterface {
+    public Document createPDFDocument(Certificate cert) throws Exception;
+
+    public boolean checkRenewal() throws Exception;
+
+    public boolean setupEmailServerProperties();
+
+    public boolean sendEmail() throws Exception;
+
+    public MimeMessage CertificateExpiredEmail(String orgName, String orgEmail) throws Exception;
+
+    public boolean organisationRenewal(String orgId) throws Exception;
+
+    public boolean adminRenewal(String orgId) throws Exception;
 }

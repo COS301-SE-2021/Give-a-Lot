773b8c87a5d483c5bc1fc01f3bc69d270b3ecb6c
Joshua <u18227369@tuks.co.za>
2021-08-05 18:51:07 +0200

    Fixing pending issues

--- backend/src/main/java/com/GiveaLot/givealot/Organisation/exceptions/OrganisationException.java
+++
@@ -1,7 +0,0 @@
-package com.GiveaLot.givealot.Organisation.exceptions;
-
-public class OrganisationException extends Exception{
-    public OrganisationException(){}
-    public OrganisationException(String message){ super(message);}
-    public OrganisationException(String message,Throwable cause){super(message,cause);}
-}

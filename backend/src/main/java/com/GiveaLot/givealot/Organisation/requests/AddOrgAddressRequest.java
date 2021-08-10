99b392755e9bcc39dd1e7dade2bc30fd62fda859
Xplodiator <33042059+Xplodiator@users.noreply.github.com>
2021-08-10 04:42:06 +0200

    Added back AddOrgAddressRequest.java

--- backend/src/main/java/com/GiveaLot/givealot/Organisation/requests/AddOrgAddressRequest.java
+++ backend/src/main/java/com/GiveaLot/givealot/Organisation/requests/AddOrgAddressRequest.java
@@ -6,10 +6,24 @@ public class AddOrgAddressRequest {
     private String orgId;
     private String address;
 
+    public AddOrgAddressRequest(String orgId, String address) {
+        this.orgId = orgId;
+        this.address = address;
+    }
+
+    public void setOrgId(String orgId) {
+        this.orgId = orgId;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     public String getOrgId() {
+        return this.orgId;
     }
 
     public String getAddress() {
+        return this.address;
     }
 }

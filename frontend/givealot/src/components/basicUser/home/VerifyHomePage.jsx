b6c63ee1ab6795e4701bbd12cd161a4acee2b8c5
WandaWeira <55826359+WandaWeira@users.noreply.github.com>
2021-08-14 12:38:58 +0200

    certificate page

--- frontend/givealot/src/components/basicUser/home/VerifyHomePage.jsx
+++ frontend/givealot/src/components/basicUser/home/VerifyHomePage.jsx
@@ -1,9 +1,35 @@
 import React, { Component } from 'react'
 import HomeHeader from "./HomeHeader";
 import "./home.css"
+import axios from 'axios'
 import Button from '@material-ui/core/Button';
 
 export class VerifyHomePage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            uploadStatus: false
+        }
+        this.handleUploadImage = this.handleUploadImage.bind(this);
+    }
+
+
+    handleUploadImage(ev) {
+        ev.preventDefault();
+
+        const data = new FormData();
+        data.append('file', this.uploadInput.files[0]);
+        data.append('filename', this.fileName.value);
+
+        axios.post('http://localhost:8000/upload', data)
+            .then(function (response) {
+                // this.setState({ imageURL: `http://localhost:8000/${body.file}`, uploadStatus: true });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
 
     render() {
         return (
@@ -14,13 +40,21 @@ export class VerifyHomePage extends Component {
                         <p>Verify Certificate</p>
                     </div>
                     <div className="inputVerify">
-                        <div className="header__input">
-                            <input placeholder="Verify Certificate" type="file" />
-                            {/*<input type="file" onChange={this.onFileChange} />*/}
+                        {/*<div className="header__input">*/}
+                        {/*    <input placeholder="Verify Certificate" type="file" />*/}
+                        {/*    /!*<input type="file" onChange={this.onFileChange} />*!/*/}
+                        {/*</div>*/}
+                        {/*<Button variant="outlined" color="primary" style={{backgroundColor:"#2ab27b" }}>*/}
+                        {/*    Verify*/}
+                        {/*</Button>*/}
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div className="form-group files color">
+                                    <label>Upload Certificate </label>
+                                    <input type="file" className="form-control" name="file" />
+                                </div>
+                            </div>
                         </div>
-                        <Button variant="outlined" color="primary" style={{backgroundColor:"#2ab27b" }}>
-                            Verify
-                        </Button>
                     </div>
                 </div>
             </div>
---
+++ frontend/givealot/src/components/organisation/orgPages/certificateOrg/Certificate.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import "./certificate.css"
+
+
+export class Certificate extends Component {
+
+    render() {
+        return (
+            <div className="certificate">
+                certificate
+            </div>
+        )
+    }
+}
+
+export default Certificate

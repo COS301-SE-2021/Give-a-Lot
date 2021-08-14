a77d3af9c7e197fdba93d2665e111e25010ea1d6
Joshua <u18227369@tuks.co.za>
2021-08-14 09:59:45 +0200

    Adding restructured files

--- backend/src/main/java/com/GiveaLot/givealot/Certificate/dao/CertificateDASTemp.java
+++ backend/src/main/java/com/GiveaLot/givealot/Certificate/dao/CertificateDASTemp.java
@@ -1,5 +1,8 @@
 package com.GiveaLot.givealot.Certificate.dao;
 
+import com.GiveaLot.givealot.Blockchain.dao.BlockchainDAOInterface;
+import com.GiveaLot.givealot.Blockchain.dao.BlockchainDASTemp;
+import com.GiveaLot.givealot.Blockchain.service.BlockchainService;
 import com.GiveaLot.givealot.Certificate.model.Certificate;
 import com.GiveaLot.givealot.Organisation.model.Organisation;
 import com.GiveaLot.givealot.Organisation.model.OrganisationPoints;
@@ -19,46 +22,114 @@ import java.io.File;
 @Repository("CertificateTemp")
 public class CertificateDASTemp implements CertificateDAOInterface{
 
-    private final JdbcTemplate jdbcTemplate;
 
     @Autowired
-    public CertificateDASTemp(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public CertificateDASTemp() {
+
     }
 
 
     @Override
-    public boolean createPDFDocument(Certificate cert, Organisation organisation, OrganisationPoints organisationPoints, String templateDirectory) throws Exception {
+    public boolean addCertificate(long orgId) throws Exception {
+        BlockchainService blockchainService = new BlockchainService(new BlockchainDASTemp());
+
+        OrganisationPoints organisationPoints = new OrganisationPoints();
+
+        organisationPoints.setPoints(0);
+
+        //query organisation, certificate
+
+        boolean certificateCreated = createPDFDocument();
+
+        if(!certificateCreated){
+            throw new Exception("Exception: Problem creating and storing certificate");
+        }
+
+        File certificate = retrieveCertificate(orgId);
+
+        String[] result = blockchainService
+                .uploadCertificate(orgId, certificate);
+
+        String certificateHash = result[0];
+        String txHash = result[1];
+
+        long index = blockchainService.findCertificateIndex(orgId);
+
+        //query to add current latest tx hash, index, certificate hash and level to blockchain table;
+
+
+
+        return true;
+    }
+
+    @Override
+    public boolean updateCertificate(long orgId) throws Exception {
+        BlockchainService blockchainService = new BlockchainService(new BlockchainDASTemp());
+
+        //query organisation, certificate, org points, blockchain
+
+        //we need to remove points from organisationpoints and add it to certificate, we need to remove certlevel from certificate and add it to blockchain
+
+        boolean certificateCreated = createPDFDocument();
+
+        if(!certificateCreated){
+            throw new Exception("Exception: Problem creating and storing certificate");
+        }
+
+        File certificate = retrieveCertificate(orgId);
+
+        String[] result = blockchainService
+                .upgradeCertificate(0,orgId, certificate,0);
+
+        String certificateHash = result[0];
+        String txHash = result[1];
+
+        //query to add current latest tx hash, certificate hash and level to blockchain table;
+
+        return true;
+    }
+
+    @Override
+    public File retrieveCertificate(long orgId) {
+
+        return null;
+    }
+
+    @Override
+    public boolean createPDFDocument(Certificate cert, Organisation organisation, OrganisationPoints organisationPoints) throws Exception {
 
         int points = organisationPoints.getPoints();
 
         String CertificateDestination = organisation.getDirectory() + "certificate/";
 
-        String templateCertificate = "";
+        String templateCertificate;
 
         if (points<20){
             //Level 0
-            templateCertificate = "/tmp/templates/certificateTemplate0";
+            templateCertificate = "location on the server";
         }
         else if (points>=20 && points<40){
             //Level 1
-            templateCertificate = "/tmp/templates/certificateTemplate1";
+            templateCertificate = "location on the server";
         }
         else if (points>=40 && points<60){
             //Level 2
-            templateCertificate = "/tmp/templates/certificateTemplate2";
+            templateCertificate = "location on the server";
         }
         else if (points>=60 && points<80){
             //Level 3
-            templateCertificate = "/tmp/templates/certificateTemplate3";
+            templateCertificate = "location on the server";
         }
         else if (points>=80 && points<100){
             //Level 4
-            templateCertificate = "/tmp/templates/certificateTemplate4";
+            templateCertificate = "location on the server";
         }
         else if (points>=100){
             //Level 5
-            templateCertificate = "/tmp/templates/certificateTemplate5";
+            templateCertificate = "location on the server";
+        }
+        else{
+            throw new Exception("Exception: Points Exceed limit");
         }
 
         /** Setup the pdf file **/
@@ -77,28 +148,20 @@ public class CertificateDASTemp implements CertificateDAOInterface{
             if (acroForm != null) {
 
                 PDField field = (PDField) acroForm.getField("name"); // Name
-                field.setValue(organisation.getOrgName());
-
-                field = (PDField) acroForm.getField("email"); // Email
-                field.setValue(organisation.getOrgEmail());
+                field.setValue(organisation.getOrgName().toUpperCase());
 
                 field = (PDField) acroForm.getField("slogan"); // Slogan
                 field.setValue(organisation.getSlogan());
 
-                field = (PDField) acroForm.getField("sector"); // Sector
-                field.setValue(organisation.getOrgSector());
-
-                field = (PDField) acroForm.getField("contact"); // Contact Person
-                field.setValue(organisation.getContactPerson());
-
-                field = (PDField) acroForm.getField("number"); // Contact Number
-                field.setValue(organisation.getContactNumber());
 
                 field = (PDField) acroForm.getField("created"); // Date Created
                 field.setValue(cert.getDateCreated());
 
                 field = (PDField) acroForm.getField("expiry"); // Date Expiry
                 field.setValue(cert.getDateExpiry());
+
+                acroForm.flatten();
+
             }
         }catch (Exception e){
             throw new Exception("Exception: unable to create certificate");
@@ -132,12 +195,12 @@ public class CertificateDASTemp implements CertificateDAOInterface{
     }
 
     @Override
-    public boolean organisationRenewal(String orgId) throws Exception {
+    public boolean organisationRenewal(long orgId) throws Exception {
         return false;
     }
 
     @Override
-    public boolean adminRenewal(String orgId) throws Exception {
+    public boolean adminRenewal(long orgId) throws Exception {
         return false;
     }
 }

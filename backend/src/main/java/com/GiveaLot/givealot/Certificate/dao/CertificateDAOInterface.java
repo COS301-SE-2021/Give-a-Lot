f17c00aa07614db8aaebd112914b68085c707ef2
Joshua <u18227369@tuks.co.za>
2021-08-14 09:59:41 +0200

    Adding restructured files

--- backend/src/main/java/com/GiveaLot/givealot/Certificate/dao/CertificateDAOInterface.java
+++ backend/src/main/java/com/GiveaLot/givealot/Certificate/dao/CertificateDAOInterface.java
@@ -6,9 +6,19 @@ import com.GiveaLot.givealot.Organisation.model.OrganisationPoints;
 import com.itextpdf.text.Document;
 
 import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 public interface CertificateDAOInterface {
-    public boolean createPDFDocument(Certificate cert, Organisation organisation, OrganisationPoints organisationPoints, String templateDirectory) throws Exception;
+
+    public boolean addCertificate(long orgId) throws Exception;
+
+    public boolean updateCertificate(long orgId) throws Exception;
+
+    public File retrieveCertificate(long orgId);
+
+    public boolean compare
+
+    public boolean createPDFDocument(Certificate cert, Organisation organisation, OrganisationPoints organisationPoints) throws Exception;
 
     public boolean checkRenewal() throws Exception;
 
@@ -18,7 +28,7 @@ public interface CertificateDAOInterface {
 
     public MimeMessage CertificateExpiredEmail(String orgName, String orgEmail) throws Exception;
 
-    public boolean organisationRenewal(String orgId) throws Exception;
+    public boolean organisationRenewal(long orgId) throws Exception;
 
-    public boolean adminRenewal(String orgId) throws Exception;
+    public boolean adminRenewal(long orgId) throws Exception;
 }

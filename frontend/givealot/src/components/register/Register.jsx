87e2c6bdbfb863bf6663bccae3e26369ef4dbad9
WandaWeira <55826359+WandaWeira@users.noreply.github.com>
2021-08-15 03:02:50 +0200

    Login and Registration minor changes

--- frontend/givealot/src/components/register/Register.jsx
+++ frontend/givealot/src/components/register/Register.jsx
@@ -1,35 +1,5 @@
-// import React, { Component } from 'react'
-// import "./register.css"
-// // import FeaturedHeader from "../featuredHeader/FeaturedHeader"
-// import { BrowserRouter as Router} from "react-router-dom";
-// // import RegisterUser from "./registerUser/RegisterUser"
-// import RegisterBody from "./RegisterBody";
-// // import RegisterOrganisation from "./registerOrganisation/RegisterOrganisation";
-// import FeaturedHeader from "../featuredHeader/FeaturedHeader";
-//
-// export class Login extends Component {
-//
-//     render() {
-//         return (
-//             <Router>
-//                 <div className="register">
-//                     <FeaturedHeader />
-//                     <div className="containerRegister">
-//                         <RegisterBody />
-//                     </div>
-//                 </div>
-//             </Router>
-//         )
-//     }
-// }
-//
-// export default Login
-
 import "./register.css"
 import FeaturedHeader from "../featuredHeader/FeaturedHeader"
-import {Switch, Route} from "react-router-dom";
-import RegisterUser from "./registerUser/RegisterUser"
-import RegisterOrganisation from "./registerOrganisation/RegisterOrganisation";
 import RegisterBody from "./RegisterBody";
 import React from "react";
 

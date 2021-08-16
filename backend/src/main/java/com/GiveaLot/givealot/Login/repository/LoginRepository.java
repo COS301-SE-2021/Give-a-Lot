3df1ac83344c941cd124c0a38bc937640ad7effa
Kgomotso404 <u19104546@tuks.co.za>
2021-08-16 19:01:32 +0200

    added LoginServiceImpl functions

--- backend/src/main/java/com/GiveaLot/givealot/Login/repository/LoginRepository.java
+++ backend/src/main/java/com/GiveaLot/givealot/Login/repository/LoginRepository.java
@@ -1,6 +1,17 @@
 package com.GiveaLot.givealot.Login.repository;
 
+import com.GiveaLot.givealot.User.dataclass.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LoginRepository extends JpaRepository {
+
+
+    @Query("SELECT s FROM User s WHERE s.email = ?1")
+    User findUserByEmail(String email);
+
+    @Query("SELECT s FROM Organisations s WHERE s.orgEmail = ?1")
+    User findOrganisationByEmail(String email);
+
+
 }

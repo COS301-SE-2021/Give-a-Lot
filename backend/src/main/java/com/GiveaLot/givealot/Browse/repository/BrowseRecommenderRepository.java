d0046f87641de0d86a07f33bcb8f40376e81e446
Xplodiator <33042059+Xplodiator@users.noreply.github.com>
2021-08-16 22:09:30 +0200

    recommender

--- backend/src/main/java/com/GiveaLot/givealot/Browse/repository/BrowseRecommenderRepository.java
+++ backend/src/main/java/com/GiveaLot/givealot/Browse/repository/BrowseRecommenderRepository.java
@@ -1,4 +1,12 @@
 package com.GiveaLot.givealot.Browse.repository;
 
-public interface BrowseRecommenderRepository {
+import com.GiveaLot.givealot.Browse.model.Browse;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface BrowseRecommenderRepository extends JpaRepository<Browse,Long>
+{
+    @Query("SELECT DISTINCT b.interactions from Browse AS b where b.orgId = ?1 AND b.sector = ?2")
+    Integer getInteractionsFromSector(Long orgId, String sector);
+
 }

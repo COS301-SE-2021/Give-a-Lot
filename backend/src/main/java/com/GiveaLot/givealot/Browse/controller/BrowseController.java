d8c24d7f3bafbdab8c0d86754433185ffdab5aa0
Xplodiator <33042059+Xplodiator@users.noreply.github.com>
2021-08-16 23:13:15 +0200

    recommender - done - not tested yet

--- backend/src/main/java/com/GiveaLot/givealot/Browse/controller/BrowseController.java
+++ backend/src/main/java/com/GiveaLot/givealot/Browse/controller/BrowseController.java
@@ -2,12 +2,11 @@ package com.GiveaLot.givealot.Browse.controller;
 
 import com.GiveaLot.givealot.Browse.response.browseOrganisationsBySectorResponse;
 import com.GiveaLot.givealot.Browse.service.BrowseServiceImp;
+import com.GiveaLot.givealot.Organisation.model.Organisations;
 import com.GiveaLot.givealot.Organisation.service.response.responseJSON;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -47,6 +46,29 @@ public class BrowseController {
             response.setMessage("Exception: browse failed due to " + e);
         }
 
+        return response;
+    }
+
+    @GetMapping("/sectors/{userId}")
+    responseJSON browseOrganisationsRecommended(@PathVariable("userId")  @NonNull Long userId)
+    {
+        response.setObject(null);
+        try
+        {
+            List<Organisations> res = service.getRecommendedOrganisations(userId);
+
+            if(res != null)
+            {
+                response.setCode("ok_org_br_200");
+                response.setMessage("success");
+                response.setObject(res);
+            }
+        }
+        catch (Exception e)
+        {
+            response.setCode("bad_org_br_500");
+            response.setMessage("Exception: browse failed due to " + e);
+        }
         return response;
     }
 }

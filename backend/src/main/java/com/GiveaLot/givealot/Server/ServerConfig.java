268a644bee90728730ebc9735a181a305c2db35d
Joshua <u18227369@tuks.co.za>
2021-08-18 12:00:14 +0200

    Fixing organisation points

---
+++ backend/src/main/java/com/GiveaLot/givealot/Server/ServerConfig.java
@@ -0,0 +1,19 @@
+package com.GiveaLot.givealot.Server;
+
+public class ServerConfig {
+    private String remoteHost = "209.97.142.151";
+    private String username = "ubuntu";
+    private String password = "COS301-DsP";
+
+    public String getRemoteHost() {
+        return remoteHost;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}

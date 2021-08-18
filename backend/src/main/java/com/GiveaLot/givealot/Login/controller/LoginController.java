da24ba1d1b1a7da46bc6962b5034641207c10c2c
Xplodiator <33042059+Xplodiator@users.noreply.github.com>
2021-08-18 22:45:41 +0200

    organisations controller revision

--- backend/src/main/java/com/GiveaLot/givealot/Login/controller/LoginController.java
+++ backend/src/main/java/com/GiveaLot/givealot/Login/controller/LoginController.java
@@ -24,6 +24,22 @@ public class LoginController {
         this.service = service;
     }
 
+    @PostMapping("/determine") /*tested - works perfect*/
+    ResponseEntity<LoginResponse> login(@RequestBody @NonNull LoginRequest body)
+    {
+        LoginResponse response;
+
+        try
+        {
+            response = service.login(body);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
+        catch (Exception e)
+        {
+            return new ResponseEntity<>(new LoginResponse(false,e.toString(),null), HttpStatus.OK);
+        }
+    }
+
     @PostMapping("/general") /*tested - works perfect*/
     ResponseEntity<LoginResponse> loginGeneralUser(@RequestBody @NonNull LoginRequest body)
     {

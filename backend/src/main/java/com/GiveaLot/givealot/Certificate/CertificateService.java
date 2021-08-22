fca7df6250d830261d7f71e08351a2c7e646b784
Kgomotso404 <u19104546@tuks.co.za>
2021-08-23 01:37:07 +0200

    added branch

--- backend/src/main/java/com/GiveaLot/givealot/Certificate/CertificateService.java
+++ backend/src/main/java/com/GiveaLot/givealot/Certificate/CertificateService.java
@@ -13,7 +13,7 @@ public interface CertificateService {
      * Generates a new Certificate .
      * @param request:createPDFCertificateRequest Object
      * @return createPDFCertificateResponse object that holds the created pdf
-     * @throws NotAuthorizedException if user is not an admin that is trying to generate report
+     * @throws NotAuthoriedException if user is not an admin that is trying to generate report
      * @throws CertificateException if Report could not be generated for some reason
      * @throws InvalidRequestException if the request object is not correct
      */

5b2d32b7797281cbd6683fac15d3d36c2b45578b
WandaWeira <55826359+WandaWeira@users.noreply.github.com>
2021-08-24 20:32:03 +0200

    Register Organisation component

---
+++ frontend/givealot/src/views/registration/registerOrganisation/RegisterOrganisation.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export class RegisterOrganisation extends Component {
+    render() {
+        return (
+            <div>
+                Register Organisation
+            </div>
+            )
+
+
+    }
+}
+
+export default RegisterOrganisation;

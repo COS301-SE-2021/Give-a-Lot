35577251fac2886336b7e5fefb1c7b46bd8fa8eb
Xplodiator <33042059+Xplodiator@users.noreply.github.com>
2021-08-25 23:42:02 +0200

    modified browse and add organisation. browse now displays the certificate level which the current organisation holds, add organisation model and service were modified to create a field for the organisation's creation date edit: added certificate level to the response.

--- backend/src/main/java/com/GiveaLot/givealot/Browse/response/browseSectorOrganisation.java
+++ backend/src/main/java/com/GiveaLot/givealot/Browse/response/browseSectorOrganisation.java
@@ -9,12 +9,22 @@ public class browseSectorOrganisation {
     private String orgName;
     private String dateAdded;
     private String imgUrl;
+    private Integer certificate_level;
 
-    public browseSectorOrganisation(Long orgId, String orgName, String dateAdded, String imgUrl) {
+    public browseSectorOrganisation(Long orgId, String orgName, String dateAdded, String imgUrl, Integer certificate_level) {
         this.orgId = orgId;
         this.orgName = orgName;
         this.dateAdded = dateAdded;
         this.imgUrl = imgUrl;
+        this.certificate_level = certificate_level;
+    }
+
+    public Integer getCertificate_level() {
+        return certificate_level;
+    }
+
+    public void setCertificate_level(Integer certificate_level) {
+        this.certificate_level = certificate_level;
     }
 
     public Long getOrgId() {

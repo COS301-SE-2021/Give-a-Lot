217a416cd58ba9b604668b819faa8de8fb920db5
TshilidziN <64367951+TshilidziN@users.noreply.github.com>
2021-08-25 04:48:19 +0200

    Created register user page

--- frontend/givealot/src/App.js
+++ frontend/givealot/src/App.js
@@ -4,7 +4,9 @@ import Browse from './views/browse/Browse';
 import Dashboard from './views/dashboard/Dashboard';
 import Login from './views/login/Login';
 import SignUp from './views/login/Components/SignUp'
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'; 
+import RegisterOrg from './views/registration/registerOrganisation/RegisterOrganisation'
+import RegisterUser from './views/registration/registerUser/RegisterUser'
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 function App()
 {
@@ -15,6 +17,8 @@ function App()
         <Route path="/browse" exact component={Browse}/>
         <Route path="/login" exact component={Login}/>
         <Route path="/signUp" exact component={SignUp}/>
+        <Route path="/registerOrg" exact component={RegisterOrg}/>
+        <Route path="/registerUser" exact component={RegisterUser}/>
         <Route path="/dashboard" exact component={Dashboard}/>
 
       </Switch>
---
+++ frontend/givealot/src/views/registration/registerUser/RegisterUser.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import "../registerUser/Styles/RegisterUser.css"
+
+
+function RegisterUser()
+{
+    return (
+        <div>
+           Regiister user
+        </div>
+    );
+}
+
+export default RegisterUser;

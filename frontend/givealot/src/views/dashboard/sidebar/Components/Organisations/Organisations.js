66a0b8904274ebeff31a3179fe13e55ff32eb6f8
WandaWeira <55826359+WandaWeira@users.noreply.github.com>
2021-08-26 23:23:17 +0200

    dashboard organisation view created

--- frontend/givealot/src/views/dashboard/sidebar/Components/Organisations/Organisations.js
+++ frontend/givealot/src/views/dashboard/sidebar/Components/Organisations/Organisations.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
 import "../../styles/Organisations.css"
+import SearchIcon from '@material-ui/icons/Search';
+import AddCircleOutlinedIcon from '@material-ui/icons/AddCircleOutlined';
 
 export class Organisations extends Component {
 
     render() {
         return (
             <div className="organisations">
-                organisations here
+                <div className="OrgWrapper">
+                    <div className="OrgSearch">
+                        <div className="header__input">
+                            <input placeholder="search organisation..." type="text" />
+                            <SearchIcon/>
+                        </div>
+                    </div>
+                    <div className="OrgAdd">
+                        Add Organisation
+                        <AddCircleOutlinedIcon/>
+                    </div>
+                    <div className="OrgBody">
+                        body
+                    </div>
+                </div>
             </div>
         )
     }

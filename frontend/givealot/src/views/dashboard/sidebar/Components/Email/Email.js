871b11957a658eba0568ffaaa0bbd15dab866d51
TshilidziN <64367951+TshilidziN@users.noreply.github.com>
2021-08-27 06:39:48 +0200

    Updated Email page

--- frontend/givealot/src/views/dashboard/sidebar/Components/Email/Email.js
+++ frontend/givealot/src/views/dashboard/sidebar/Components/Email/Email.js
@@ -6,7 +6,58 @@ export class Email extends Component {
     render() {
         return (
             <div className="email">
-                featured here
+               <div className="email-container">
+
+                   <div className="container">
+                       <form >
+                           <div className="row">
+                               <div className="col-25">
+                                   <label >First Name</label>
+                               </div>
+                               <div className="col-75">
+                                   <input
+                                       type="text"
+                                       id="fname"
+                                       name="firstname"
+                                       placeholder="Your name.."
+                                   />
+                               </div>
+                           </div>
+                           <div className="row">
+                               <div className="col-25">
+                                   <label >Last Name</label>
+                               </div>
+                               <div className="col-75">
+                                   <input
+
+                                       type="text"
+                                       id="lname"
+                                       name="lastname"
+                                       placeholder="Your last name.."
+                                   />
+                               </div>
+                           </div>
+
+                           <div className="row">
+                               <div className="col-25">
+                                   <label >Subject</label>
+                               </div>
+                               <div className="col-75">
+                                   <textarea
+                                       id="subject"
+                                       name="subject"
+                                       placeholder="Write something.."
+                                       style={{height:"200px"}}/>
+
+                               </div>
+                           </div>
+                           <div className="row">
+                               <input type="submit" value="Submit"/>
+                           </div>
+                       </form>
+                   </div>
+
+               </div>
             </div>
         )
     }

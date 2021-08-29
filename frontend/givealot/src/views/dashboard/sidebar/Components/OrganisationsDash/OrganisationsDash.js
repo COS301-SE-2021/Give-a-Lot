c4753b0fed5321f2832acfb6d8a575a11c05d403
WandaWeira <55826359+WandaWeira@users.noreply.github.com>
2021-08-29 02:29:02 +0200

    organisations page

--- frontend/givealot/src/views/dashboard/sidebar/Components/OrganisationsDash/OrganisationsDash.js
+++ frontend/givealot/src/views/dashboard/sidebar/Components/OrganisationsDash/OrganisationsDash.js
@@ -19,6 +19,11 @@ import { DataGrid } from '@material-ui/data-grid';
 import EditIcon from '@material-ui/icons/Edit';
 import IconButton from '@material-ui/core/IconButton';
 import { Link } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
+import AddCircleOutlinedIcon from "@material-ui/icons/AddCircleOutlined";
+import Button from '@material-ui/core/Button';
+
+import "../../styles/Organisations.css"
 
 export default function OrganisationsDash() {
     const [isLoaded,setIsLoaded] = useState(false);
@@ -37,10 +42,10 @@ export default function OrganisationsDash() {
     }, []);
     const columns = [
         { field: "id", headerName: "ID", width: 100 },
-    { field: 'name', headerName: 'Name', width: 200 },
-    { field: 'username', headerName: 'User name', width: 200 },
-    { field: 'email', headerName: 'EMail', width: 200 },
-    { field: 'website', headerName: 'Website', width: 200 },
+        { field: 'name', headerName: 'Name', width: 200 },
+        { field: 'username', headerName: 'User name', width: 200 },
+        { field: 'email', headerName: 'EMail', width: 200 },
+        { field: 'website', headerName: 'Website', width: 200 },
         {
             field: "action",
             headerName: "Action",
@@ -61,6 +66,20 @@ export default function OrganisationsDash() {
 ];
     return(
         <div className="OrganisationsDash">
+            {/*<p> Organisations </p>*/}
+            <div className="OrgAdd">
+                <Button variant="contained" className="buttonAdd">
+                    Add Organisation
+                    <AddCircleOutlinedIcon/>
+                </Button>
+                <div>
+                    <div className="header__input">
+                        <input placeholder="search organisation" type="text" />
+                        <SearchIcon/>
+                    </div>
+                </div>
+            </div>
+
             <div className="table">
                 <div style={{ display: 'flex', height: '100%' }}>
                     <div style={{ flexGrow: 1 }}>

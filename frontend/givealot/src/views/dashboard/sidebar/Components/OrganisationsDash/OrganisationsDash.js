9a93db3c73984d2db5a647a9ee8aae15c9e94427
WandaWeira <55826359+WandaWeira@users.noreply.github.com>
2021-08-29 13:21:48 +0200

    View organisations page

--- frontend/givealot/src/views/dashboard/sidebar/Components/OrganisationsDash/OrganisationsDash.js
+++ frontend/givealot/src/views/dashboard/sidebar/Components/OrganisationsDash/OrganisationsDash.js
@@ -15,58 +15,20 @@
 // export default OrganisationsDash
 
 import React, {useState, useEffect} from "react";
-import { DataGrid } from '@material-ui/data-grid';
-import EditIcon from '@material-ui/icons/Edit';
-import IconButton from '@material-ui/core/IconButton';
 import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
 import AddCircleOutlinedIcon from "@material-ui/icons/AddCircleOutlined";
-import Button from '@material-ui/core/Button';
-
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
 import "../../styles/Organisations.css"
 
 export default function OrganisationsDash() {
-    const [isLoaded,setIsLoaded] = useState(false);
-    const [rowData,setRowData] = useState([]);
-    useEffect(() => {
-        const axios = require('axios').default;
-        axios
-            .get('http://jsonplaceholder.typicode.com/users')
-            .then((response) => {
-            setIsLoaded(true);
-            console.log(response.data);
-            setRowData(response.data);
-            response.data.forEach(user => {
-            });
-        });
-    }, []);
-    const columns = [
-        { field: "id", headerName: "ID", width: 100 },
-        { field: 'name', headerName: 'Name', width: 200 },
-        { field: 'username', headerName: 'User name', width: 200 },
-        { field: 'email', headerName: 'EMail', width: 200 },
-        { field: 'website', headerName: 'Website', width: 200 },
-        {
-            field: "action",
-            headerName: "Action",
-            width: 150,
-            renderCell: (params) => {
-                return (
-                    <>
-                        <Link to={"/org/" + params.row.id}>
-                        {/*    <button className="OrgListEdits">Edit</button>*/}
-                            <IconButton>
-                                <EditIcon />
-                            </IconButton>
-                        </Link>
-                    </>
-                );
-            },
-        },
-];
     return(
         <div className="OrganisationsDash">
-            {/*<p> Organisations </p>*/}
             <div className="OrgAdd">
                 <Button variant="contained" className="buttonAdd">
                     Add Organisation
@@ -81,17 +43,44 @@ export default function OrganisationsDash() {
             </div>
 
             <div className="table">
-                <div style={{ display: 'flex', height: '100%' }}>
-                    <div style={{ flexGrow: 1 }}>
-                        <DataGrid
-                            columns={columns}
-                            rows={rowData}
-                            id='id'
-                            pageSize={15}
-                        />
-                    </div>
-                </div>
+                <Card style={{margin: "1em"}}>
+                    <CardMedia style={{height: 0}}
+                               // image={props.course.fields.courseImage.fields.file.url}
+                               // title={props.course.fields.title}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="headline" component="h4">
+                            Organisation Name
+                        </Typography>
+                        <Typography style={{display: "flex", alignContent: "center", justifyContent: "space-between"}}>
+                            <Typography>
+                                <Typography component="p">
+                                    Email
+                                </Typography>
+                                {/*<Typography component="p" secondary= "email" />*/}
+                            </Typography>
 
+                            <Typography component="p">
+                                Contact Person
+                            </Typography>
+                            <Typography component="p">
+                                Contact Number
+                            </Typography>
+                            <Typography component="p">
+                                Sector
+                            </Typography>
+                            <Typography component="p">
+                                Status
+                            </Typography>
+                        </Typography>
+                    </CardContent>
+
+                    <CardActions>
+                        <Button size="small" color="primary"  target="_blank">
+                            View
+                        </Button>
+                    </CardActions>
+                </Card>
             </div>
         </div>
     );

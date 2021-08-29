c17848940fc8e39a043fe716068522f698caec14
WandaWeira <55826359+WandaWeira@users.noreply.github.com>
2021-08-29 18:05:26 +0200

    Calendar page

--- frontend/givealot/src/views/dashboard/sidebar/Components/Calendar/Calendar.js
+++ frontend/givealot/src/views/dashboard/sidebar/Components/Calendar/Calendar.js
@@ -1,16 +1,36 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+// import "./styles/Calendar.css"
+//
+// export class Calendar extends Component {
+//
+//
+//     render() {
+//         return (
+//             <div className="calendar">
+//                 calendar here
+//             </div>
+//         )
+//     }
+// }
+//
+// export default Calendar
+
+import React from 'react'
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
 import "./styles/Calendar.css"
 
-export class Calendar extends Component {
-
-
+export default class DemoApp extends React.Component {
     render() {
         return (
             <div className="calendar">
-                calendar here
+                <FullCalendar dateClick={this.handleDateClick} plugins={[ dayGridPlugin, interactionPlugin ]} />
             </div>
         )
     }
-}
 
-export default Calendar
+    handleDateClick = (arg) => { // bind with an arrow function
+        alert(arg.dateStr)
+    }
+}

33b0439ad45738fbac8514e3939c0afe1afc5f3e
WandaWeira <55826359+WandaWeira@users.noreply.github.com>
2021-08-31 00:20:00 +0200

    view validate information loop finalised

--- frontend/givealot/src/views/dashboard/sidebar/Components/Validate/Validate.js
+++ frontend/givealot/src/views/dashboard/sidebar/Components/Validate/Validate.js
@@ -5,10 +5,37 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
+import axios from "axios";
 
 export class Validate extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            valid:[],
+            error: "",
+        }
+    }
+    componentDidMount(){
+        let config = {
+            headers: {
+                "Content-Type": "application/json",
+                'Access-Control-Allow-Origin': '*',
+            }
+        }
+        axios.get('http://jsonplaceholder.typicode.com/users',  config)
+            .then(response =>{
+                this.setState({valid: response.data})
+                console.log(this.state.valid)
+            })
+            .catch(error =>{
+                console.log(error)
+                this.setState({error : 'Error Retrieving data'})
+            })
+    }
+
     render() {
+        const { valid } = this.state
         return (
             <div className="validate">
                 <div className="validateInfo">
@@ -16,11 +43,13 @@ export class Validate extends Component {
                 </div>
 
                 <div className="table">
+                    {valid.map((item, index) =>{
+                        return(
                     <Card style={{margin: "1em"}}>
                         <CardContent>
                             <Typography className="valid">
                                 <Typography component="p">
-                                    Organisation Name
+                                    {item.name}
                                 </Typography>
                                 <Link to={"/orgValidate"} className="link">
                                     <Button size="small" variant="outlined" className="buttonValid" >
@@ -36,6 +65,8 @@ export class Validate extends Component {
                             </Typography>
                         </CardContent>
                     </Card>
+                        )
+                    })}
                 </div>
             </div>
         )

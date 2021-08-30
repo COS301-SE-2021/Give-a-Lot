b893d034c397275af670f88e0ae1a739d6d214a5
TshilidziN <64367951+TshilidziN@users.noreply.github.com>
2021-08-30 07:55:38 +0200

    Updated level 2 form css

--- frontend/givealot/src/views/dashboard/sidebar/Components/Certificate/Upgrade3.js
+++ frontend/givealot/src/views/dashboard/sidebar/Components/Certificate/Upgrade3.js
@@ -1,29 +1,42 @@
 import React, {Component} from 'react';
 import "./Style/Certificate.css";
-
-import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
-import Button from '@material-ui/core/Button';
 import 'date-fns';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
-import { withStyles } from '@material-ui/core/styles'
+import { withStyles ,makeStyles } from '@material-ui/core/styles'
 import {toast, ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import 'react-datepicker/dist/react-datepicker.css';
+import StarOutlineIcon from '@material-ui/icons/StarOutline';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import TextField from "@material-ui/core/TextField";
+import { purple } from '@material-ui/core/colors';
 import Axios from "axios";
 
 const styles = theme => ({
-    button: {
-        margin: theme.spacing(1),
+
+    root: {
+        display: "flex",
+        flexWrap: "wrap"
     },
+    textField: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        width: "25ch"
+    },
+
 });
+
 
 export class Upgrade3 extends Component {
 
     constructor (props) {
         super(props)
         this.state={
-            level: 0,
+            level: 3,
 
         };
     }
@@ -32,11 +45,75 @@ export class Upgrade3 extends Component {
     render(){
         const { classes } = this.props;
 
+
+
         return (
             <div className="upgrade">
+                <div className="upgradeTitle">
+                    <p className="upgradeTitle1">Current level:</p>
+                    <p className="upgradeTitle2">One</p>
+                </div>
+
+                <div className="progress">
+                    <div className="progress_complete"/>
+                    <div className="progress_complete"/>
+                    <div className="progress_complete"/>
+                    <div className="progress_empty"/>
+                    <div className="progress_empty"/>
+                    <div className="progress6"> <StarOutlineIcon fontSize="large"/></div>
+                </div>
+
+                <Card className="upgrade_card" variant="outlined">
+                    <CardContent>
+                        <div className={classes.root}>
+                            <form>
+                                <div>
+                                    <TextField
+                                        id="outlined-full-width"
+                                        label="Website"
+                                        style={{ margin: 8 }}
+                                        placeholder="Enter your website url.."
+                                        helperText="Full width!"
+                                        fullWidth
+                                        margin="normal"
+                                        InputLabelProps={{
+                                            shrink: true,
+                                        }}
+                                        variant="outlined"
+                                    />
+                                    <TextField
+                                        id="outlined-full-width"
+                                        label="Address"
+                                        style={{ margin: 8 }}
+                                        placeholder="Enter your address"
+                                        helperText="Full width!"
+                                        fullWidth
+                                        margin="normal"
+                                        InputLabelProps={{
+                                            shrink: true,
+                                        }}
+                                        variant="outlined"
+                                    />
+                                </div>
+                                <div className="upgrade_Button">
+                                    <button className="upgrade-btn" type="submit">
+                                        Submit
+                                    </button>
+                                </div>
+
+                            </form>
+                        </div>
+
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Learn More</Button>
+                    </CardActions>
+                </Card>
+
 
             </div>
         );
     }
 }
 export default withStyles(styles)(Upgrade3);
+

1769276a45ee8989008b92f9ef4585abd48ad489
WandaWeira <55826359+WandaWeira@users.noreply.github.com>
2021-08-31 08:56:06 +0200

    calendar updated

--- frontend/givealot/src/views/dashboard/sidebar/Components/Calendar/Calendar.js
+++ frontend/givealot/src/views/dashboard/sidebar/Components/Calendar/Calendar.js
@@ -16,21 +16,111 @@
 // export default Calendar
 
 import React from 'react'
-import FullCalendar from '@fullcalendar/react' // must go before plugins
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
-import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
+import Paper from '@material-ui/core/Paper';
+import { ViewState, EditingState } from '@devexpress/dx-react-scheduler';
+import {
+    Scheduler,
+    Appointments,
+    AppointmentForm,
+    AppointmentTooltip,
+    WeekView,
+    EditRecurrenceMenu,
+    AllDayPanel,
+    ConfirmationDialog,
+} from '@devexpress/dx-react-scheduler-material-ui';
+
 import "./styles/Calendar.css"
+import { appointments } from "./data"
 
 export default class DemoApp extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: appointments,
+            currentDate: '2018-06-27',
+
+            addedAppointment: {},
+            appointmentChanges: {},
+            editingAppointment: undefined,
+        };
+
+        this.commitChanges = this.commitChanges.bind(this);
+        this.changeAddedAppointment = this.changeAddedAppointment.bind(this);
+        this.changeAppointmentChanges = this.changeAppointmentChanges.bind(this);
+        this.changeEditingAppointment = this.changeEditingAppointment.bind(this);
+    }
+
+    changeAddedAppointment(addedAppointment) {
+        this.setState({ addedAppointment });
+    }
+
+    changeAppointmentChanges(appointmentChanges) {
+        this.setState({ appointmentChanges });
+    }
+
+    changeEditingAppointment(editingAppointment) {
+        this.setState({ editingAppointment });
+    }
+
+    commitChanges({ added, changed, deleted }) {
+        this.setState((state) => {
+            let { data } = state;
+            if (added) {
+                const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
+                data = [...data, { id: startingAddedId, ...added }];
+            }
+            if (changed) {
+                data = data.map(appointment => (
+                    changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment));
+            }
+            if (deleted !== undefined) {
+                data = data.filter(appointment => appointment.id !== deleted);
+            }
+            return { data };
+        });
+    }
+
     render() {
+        const {
+            currentDate, data, addedAppointment, appointmentChanges, editingAppointment,
+        } = this.state;
         return (
             <div className="calendar">
-                <FullCalendar dateClick={this.handleDateClick} plugins={[ dayGridPlugin, interactionPlugin ]} />
+                <Paper>
+                    <Scheduler
+                        data={data}
+                        height={660}
+                    >
+                        <ViewState
+                            currentDate={currentDate}
+                        />
+                        <EditingState
+                            onCommitChanges={this.commitChanges}
+                            addedAppointment={addedAppointment}
+                            onAddedAppointmentChange={this.changeAddedAppointment}
+                            appointmentChanges={appointmentChanges}
+                            onAppointmentChangesChange={this.changeAppointmentChanges}
+                            editingAppointment={editingAppointment}
+                            onEditingAppointmentChange={this.changeEditingAppointment}
+                        />
+                        <WeekView
+                            startDayHour={9}
+                            endDayHour={17}
+                        />
+                        <AllDayPanel />
+                        <EditRecurrenceMenu />
+                        <ConfirmationDialog />
+                        <Appointments />
+                        <AppointmentTooltip
+                            showOpenButton
+                            showDeleteButton
+                        />
+                        <AppointmentForm />
+                    </Scheduler>
+                </Paper>
             </div>
         )
     }
 
-    handleDateClick = (arg) => { // bind with an arrow function
-        alert(arg.dateStr)
-    }
 }
--- frontend/givealot/src/views/dashboard/sidebar/Components/Validate/OrgValidate.js
+++ frontend/givealot/src/views/dashboard/sidebar/Components/Validate/OrgValidate.js
@@ -6,36 +6,73 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import CardActions from '@material-ui/core/CardActions';
+import {Link} from "react-router-dom";
+import axios from "axios";
 
 export class OrgValidate extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            validation:[],
+            error: "",
+        }
+    }
+    componentDidMount(){
+        let config = {
+            headers: {
+                "Content-Type": "application/json",
+                'Access-Control-Allow-Origin': '*',
+            }
+        }
+        // const adminUsersRequestBody = {
+        //     "adminUserEmail" : this.state.adminUserEmail
+        // }
+        axios.get('http://jsonplaceholder.typicode.com/users',  config)
+            .then(response =>{
+                // console.log(response)
+                this.setState({validation: response.data})
+                console.log(this.state.validation)
+            })
+            .catch(error =>{
+                console.log(error)
+                this.setState({error : 'Error Retrieving data'})
+            })
+    }
+
+
     render() {
+        const { validation } = this.state
         return (
             <div className="validate">
                 <div className="validBody">
-                    <Card>
-                        <CardContent>
-                            <div  className="name">
-                                Organisation Name
-                            </div>
-                            <Typography className="validBody">
-                                <Typography>
-                                    Organisation Name
-                                </Typography>
-                                <Typography>
-                                    Organisation Name
-                                </Typography>
-                            </Typography>
-                            <CardActions style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
-                                <Button variant="contained" color="primary">
-                                    Verify
-                                </Button>
-                                <Button variant="contained" color="secondary">
-                                    Deny
-                                </Button>
-                            </CardActions>
-                        </CardContent>
-                    </Card>
+                    <div  className="name">
+                        Organisation Name
+                    </div>
+                    <div className="table">
+                        {validation.map((item, index) =>{
+                            return(
+                                <Card style={{margin: "1em"}}>
+                                    <CardContent>
+                                        <Typography className="valid">
+                                            <Typography component="p">
+                                                {item.name}
+                                            </Typography>
+                                            <Typography style={{display: "flex", alignItems: "center", justifyContent: "space-between"}}>
+                                                <Button variant="contained" className="buttonValidView">
+                                                    Accept
+                                                </Button>
+                                                <Button variant="contained" className="buttonValidViewDeny">
+                                                    Deny
+                                                </Button>
+                                            </Typography>
+
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            )
+                        })}
+                    </div>
                 </div>
 
             </div>

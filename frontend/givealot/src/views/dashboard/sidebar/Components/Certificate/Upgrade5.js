9881901b6fca72d800dd94ce485083138e1c4034
TshilidziN <64367951+TshilidziN@users.noreply.github.com>
2021-08-31 02:44:13 +0200

    created level 5 page

---
+++ frontend/givealot/src/views/dashboard/sidebar/Components/Certificate/Upgrade5.js
@@ -0,0 +1,111 @@
+import React, {Component} from 'react';
+import "./Style/Certificate.css";
+import 'date-fns';
+import "react-datepicker/dist/react-datepicker.css";
+import { withStyles ,makeStyles } from '@material-ui/core/styles'
+import {toast, ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import 'react-datepicker/dist/react-datepicker.css';
+import StarOutlineIcon from '@material-ui/icons/StarOutline';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import TextField from "@material-ui/core/TextField";
+import Axios from "axios";
+
+const styles = theme => ({
+
+    root: {
+        display: "flex",
+        flexWrap: "wrap",
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                borderColor: '#957b9e',
+                borderWidth: 2
+            },
+        },
+    },
+    textField: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        width: "30ch",
+
+
+    },
+
+});
+
+
+export class Upgrade5 extends Component {
+
+    constructor (props) {
+        super(props)
+        this.state={
+            orgId:"",
+            website: "",
+            address:"",
+
+        };
+    }
+
+    handleChange = TextField => e => {
+
+        this.setState({ [TextField]: e.target.value });
+
+    };
+
+    handleFormSubmit = e => {
+        e.preventDefault();
+        const data = {
+            orgId: this.state.orgId,
+            website: this.state.website,
+            address: this.state.address,
+        };
+        Axios
+            .post("", data)
+            .then(res => console.log(res))
+            .catch(err => console.log(err));
+    };
+
+    onToast = () => {
+        toast.success('Submit successful',{
+            position: toast.POSITION.TOP_RIGHT
+
+        });
+    }
+
+
+    render(){
+        const { classes } = this.props;
+
+
+
+        return (
+            <div className="upgrade">
+                <div className="upgradeTitle">
+                    <p className="upgradeTitle1">Current level:</p>
+                    <p className="upgradeTitle2">One</p>
+                </div>
+
+                <div className="progress">
+                    <div className="progress_complete"/>
+                    <div className="progress_empty"/>
+                    <div className="progress_empty"/>
+                    <div className="progress_empty"/>
+                    <div className="progress_empty"/>
+                    <div className="progress6"> <StarOutlineIcon fontSize="large"/></div>
+                </div>
+
+                <Card className="upgrade_card" variant="outlined">
+                    <CardContent>
+
+
+                    </CardContent>
+
+                </Card>
+
+
+            </div>
+        );
+    }
+}
+export default withStyles(styles)(Upgrade5);

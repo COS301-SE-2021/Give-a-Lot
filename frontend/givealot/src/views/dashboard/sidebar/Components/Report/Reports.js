e3948382497c58301facf06210f782c0e2bab49f
TshilidziN <64367951+TshilidziN@users.noreply.github.com>
2021-09-02 07:48:19 +0200

    reports request

--- frontend/givealot/src/views/dashboard/sidebar/Components/Report/Reports.js
+++ frontend/givealot/src/views/dashboard/sidebar/Components/Report/Reports.js
@@ -1,18 +1,42 @@
 import React, {Component} from 'react'
 import "../Report/Style/Reports.css"
 import Accordions from "../Report/Accordions"
+import axios from "axios";
 
 
 
 export class Reports extends Component {
-    state={
-        count: 4
-    };
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 4,
+            reports: [],
+            error: "",
+        }
+    }
+    componentDidMount(){
+        let config = {
+            headers: {
+                "Content-Type": "application/json",
+                'Access-Control-Allow-Origin': '*',
+            }
+        }
+        axios.get('http://jsonplaceholder.typicode.com/users',  config)
+            .then(response =>{
+                this.setState({reports: response.data})
+                console.log(this.state.reports)
+            })
+            .catch(error =>{
+                console.log(error)
+                this.setState({error : 'Error Retrieving data'})
+            })
+    }
 
 
+    render() {
 
-    render()
-    {
+        const { reports } = this.state
         if (this.state.count===0){
             return (
                 <div className="non">
@@ -28,41 +52,16 @@ export class Reports extends Component {
                         Reports list
                     </div>
                     <div className="accordion">
-                        <Accordions
-                            id="Report #1"
-                            title="Fraud report"
-                            description=" Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar
-                        diam eros in elit. Pellentesque convallis laoreet laoreet.
 
-                        Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar
-                        diam eros in elit. Pellentesque convallis laoreet laoreet."
-                        />
-
-                        <Accordions
-                            id="Report #2"
-                            title="Scam report"
-                            description="But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and
-                    I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth,
-                     the master-builder of human happiness. No one rejects, "
-                        />
-
-                        <Accordions
-                            id="Report #3"
-                            title="Fraud report"
-                            description=" dislikes, or avoids pleasure itself, because it is pleasure, but because
-                      those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.
-                       Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain,
-                        but because occasionally circumstances occur in which toil and pain can procure him some great pleasure."
-                        />
-                        <Accordions
-                            id="Report #3"
-                            title="Fraud report"
-                            description=" dislikes, or avoids pleasure itself, because it is pleasure, but because
-                      those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.
-                       Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain,
-                        but because occasionally circumstances occur in which toil and pain can procure him some great pleasure."
-                        />
-
+                        {reports.map((item, index) =>{
+                            return(
+                                <Accordions
+                                    id={item.id}
+                                    title={item.name}
+                                    description={item.email}
+                                />
+                                )
+                        })}
 
                     </div>
 

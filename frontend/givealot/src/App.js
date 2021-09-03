ee6e476afdafd9206b0d9cdf58c5b65db2133d22
TshilidziN <64367951+TshilidziN@users.noreply.github.com>
2021-09-04 01:30:10 +0200

    Upgrade level 0 request

--- frontend/givealot/src/App.js
+++ frontend/givealot/src/App.js
@@ -2,31 +2,31 @@ import './App.css';
 import Home from './views/home/Home';
 import Browse from './views/browse/Browse';
 import Dashboard from './views/dashboard/Dashboard';
-import Login from './views/login/Login';
+/*import Login from './views/login/Login';
 import SignUp from './views/registration/SignUp'
 import RegisterOrg from './views/registration/registerOrganisation/RegisterOrganisation'
 import RegisterUser from './views/registration/registerUser/RegisterUser'
 import VerifyCertificate from './views/verifyCertificate/VerifyCertificate'
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
-import ViewOrganisation from "./views/browse/Components/Organisation/ViewOrganisation";
+import ViewOrganisation from "./views/browse/Components/Organisation/ViewOrganisation";*/
 
 
 function App()
 {
   return(
-    // <Router>
-    //   <Switch>
-    //     <Route path="/" exact component={Home}/>
-    //     <Route path="/browse" exact component={Browse}/>
-    //     <Route path="/login" exact component={Login}/>
-    //     <Route path="/signUp" exact component={SignUp}/>
-    //     <Route path="/registerOrg" exact component={RegisterOrg}/>
-    //     <Route path="/registerUser" exact component={RegisterUser}/>
-    //     <Route path="/dashboard" exact component={Dashboard}/>
-    //     <Route path="/organisation/:id" exact component={ViewOrganisation} />
-    //     <Route path="/verifyCertificate" exact component={VerifyCertificate} />
-    //   </Switch>
-    // </Router>
+/*     <Router>
+       <Switch>
+         <Route path="/" exact component={Home}/>
+         <Route path="/browse" exact component={Browse}/>
+         <Route path="/login" exact component={Login}/>
+         <Route path="/signUp" exact component={SignUp}/>
+         <Route path="/registerOrg" exact component={RegisterOrg}/>
+         <Route path="/registerUser" exact component={RegisterUser}/>
+         <Route path="/dashboard" exact component={Dashboard}/>
+         <Route path="/organisation/:id" exact component={ViewOrganisation} />
+         <Route path="/verifyCertificate" exact component={VerifyCertificate} />
+       </Switch>
+     </Router>*/
       <Dashboard />
   );
 }

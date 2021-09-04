1d80f9245e3b969b875b235aa0beda03a1bf833f
WandaWeira <55826359+WandaWeira@users.noreply.github.com>
2021-09-04 18:54:48 +0200

    Add Sector Api done

--- frontend/givealot/src/views/dashboard/sidebar/Components/Featured/Featured.js
+++ frontend/givealot/src/views/dashboard/sidebar/Components/Featured/Featured.js
@@ -2,12 +2,112 @@ import React, { Component } from 'react'
 import "./Styles/Featured.css"
 import Chart from "./Chart"
 import Cards from "./Cards"
-import { userData} from "../../../../../DummyData";
+// import { userData} from "../../../../../DummyData";
 import axios from "axios";
 
 export class Featured extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            UsersPerMonth: '',
+            OrganisationsPerMonth: '',
+            userData: [],
+            adminUserEmail: "admin@email.com",
+
+        }
+    }
+
+    componentDidMount(){
+        this.OrganisationsPerMonth();
+        // this.getOrganisations();
+    }
+
+    OrganisationsPerMonth(){
+        let config = {
+            headers: {
+                "Content-Type": "application/json",
+                'Access-Control-Allow-Origin': '*',
+            }
+        }
+        const OrgPerMonth = {
+            // "orgId" : this.state.orgId
+            "adminUserEmail":this.state.adminUserEmail
+        }
+        axios.post('http://localhost:8080/v1/organisation/get/num_organisations/per_month', OrgPerMonth, config)
+            .then(response =>{
+                console.log(response)
+                this.setState({ userData:[
+                        {
+                            name: "Jan",
+                            "Active User": 0,
+                        },
+                        {
+                            name: "Feb",
+                            "Active User": 0,
+                        },
+                        {
+                            name: "Mar",
+                            "Active User": 0,
+                        },
+                        {
+                            name: "Apr",
+                            "Active User": 0,
+                        },
+                        {
+                            name: "May",
+                            "Active User": 0,
+                        },
+                        {
+                            name: "Jun",
+                            "Active User": 0,
+                        },
+                        {
+                            name: "Jul",
+                            "Active User": 0,
+                        },
+                        {
+                            name: "Aug",
+                            "Active User": 0,
+                        },
+                        {
+                            name: "Sep",
+                            "Active User": 0,
+                        },
+                        {
+                            name: "Oct",
+                            "Active User": 0,
+                        },
+                        {
+                            name: "Nov",
+                            "Active User": 0,
+                        },
+                        {
+                            name: "Dec",
+                            "Active User": 0,
+                        }
+                    ]
+
+            })
+                // this.setState({ userData: response.data })
+                // console.log(this.state.Users)
+
+            })
+            .catch(error =>{
+                // console.log(error)
+                this.setState({error : 'Error Retrieving data'})
+            })
+    }
+
+    ////////////////////////////////////////
+
+
+
+    ////////////////////////////////////////
+
     render() {
-        // const { Users , Organisations, Reports} = this.state
+        const { userData } = this.state
         return (
             <div className="featured">
                 <div className="featuredBody">

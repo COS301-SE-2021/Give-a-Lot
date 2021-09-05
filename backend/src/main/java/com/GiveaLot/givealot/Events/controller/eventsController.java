a3165ed0981ef2cb858fbb31300fa37b61dfe2c8
Xplodiator <33042059+Xplodiator@users.noreply.github.com>
2021-09-06 00:05:05 +0200

    calender Add event API

--- backend/src/main/java/com/GiveaLot/givealot/Events/controller/eventsController.java
+++ backend/src/main/java/com/GiveaLot/givealot/Events/controller/eventsController.java
@@ -1,7 +1,6 @@
 package com.GiveaLot.givealot.Events.controller;
 
-import com.GiveaLot.givealot.Events.requests.addCalenderEventRequest;
-import com.GiveaLot.givealot.Events.requests.addTimeLineEventRequest;
+import com.GiveaLot.givealot.Events.requests.*;
 import com.GiveaLot.givealot.Events.response.generalResponse;
 import com.GiveaLot.givealot.Events.service.eventsServiceImp;
 import com.GiveaLot.givealot.Organisation.service.response.responseJSON;
@@ -28,12 +27,89 @@ public class eventsController {
         }
         catch (Exception e)
         {
-            return new ResponseEntity<>(new generalResponse("add_calender_event_bad", "failed"), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new generalResponse("add_calender_event_bad", "failed " + e), HttpStatus.BAD_REQUEST);
         }
     }
 
+    @PostMapping("/calender/edit/title")
+    ResponseEntity<generalResponse> editEventTitle(@RequestBody @NonNull editEventTitleRequest body)
+    {
+        try
+        {
+            return new ResponseEntity<>(service.editEventTitle(body), HttpStatus.OK);
+        }
+        catch (Exception e)
+        {
+            return new ResponseEntity<>(new generalResponse("edit_calender_event_bad", "failed " + e), HttpStatus.BAD_REQUEST);
+        }
+    }
 
-    @GetMapping("/get/timeline/{orgId}")
+    @PostMapping("/calender/edit/description")
+    ResponseEntity<generalResponse> editEventDescription(@RequestBody @NonNull editEventDescription body)
+    {
+        try
+        {
+            return new ResponseEntity<>(service.editEventDescription(body), HttpStatus.OK);
+        }
+        catch (Exception e)
+        {
+            return new ResponseEntity<>(new generalResponse("edit_calender_event_bad", "failed " + e), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/calender/edit/time/start")
+    ResponseEntity<generalResponse> editEventStartTime(@RequestBody @NonNull editTimeRequest body)
+    {
+        try
+        {
+            return new ResponseEntity<>(service.editEventStartTime(body), HttpStatus.OK);
+        }
+        catch (Exception e)
+        {
+            return new ResponseEntity<>(new generalResponse("edit_calender_event_bad", "failed " + e), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/calender/edit/time/end")
+    ResponseEntity<generalResponse> editEventEndTime(@RequestBody @NonNull editTimeRequest body)
+    {
+        try
+        {
+            return new ResponseEntity<>(service.editEventEndTime(body), HttpStatus.OK);
+        }
+        catch (Exception e)
+        {
+            return new ResponseEntity<>(new generalResponse("edit_calender_event_bad", "failed " + e), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/calender/edit/date/start")
+    ResponseEntity<generalResponse> editEventStartDate(@RequestBody @NonNull editEventDateRequest body)
+    {
+        try
+        {
+            return new ResponseEntity<>(service.editEventStartDate(body), HttpStatus.OK);
+        }
+        catch (Exception e)
+        {
+            return new ResponseEntity<>(new generalResponse("edit_calender_event_bad", "failed " + e), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/calender/edit/date/end")
+    ResponseEntity<generalResponse> editEventEndDate(@RequestBody @NonNull editEventDateRequest body)
+    {
+        try
+        {
+            return new ResponseEntity<>(service.editEventEndDate(body), HttpStatus.OK);
+        }
+        catch (Exception e)
+        {
+            return new ResponseEntity<>(new generalResponse("edit_calender_event_bad", "failed " + e), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/get/timeline/{orgId}") /*timeline*/
     ResponseEntity<responseJSON> getAllEventsForOrganisation(@PathVariable("orgId") Long orgId)
     {
         try
@@ -46,7 +122,20 @@ public class eventsController {
         }
     }
 
-    @PostMapping("/add/timeline/")
+    @GetMapping("/get/calender/{email}") /*timeline*/
+    ResponseEntity<responseJSON> getCalenderEvents(@PathVariable("email") String emailAddress)
+    {
+        try
+        {
+            return new ResponseEntity<>(service.getCalenderEvents(emailAddress), HttpStatus.OK);
+        }
+        catch (Exception e)
+        {
+            return new ResponseEntity<>(new responseJSON("get_cal_events_bad", "failed", null), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/add/timeline/") /*timeline*/
     ResponseEntity<generalResponse> addTimelineEvent(@RequestBody addTimeLineEventRequest body)
     {
         try

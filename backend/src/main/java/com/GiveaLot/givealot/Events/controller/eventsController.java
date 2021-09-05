5f9de78c4af93d84e7c720664c5049103252d90a
Xplodiator <33042059+Xplodiator@users.noreply.github.com>
2021-09-05 21:38:15 +0200

    calender Add event API

--- backend/src/main/java/com/GiveaLot/givealot/Events/controller/eventsController.java
+++ backend/src/main/java/com/GiveaLot/givealot/Events/controller/eventsController.java
@@ -1,5 +1,6 @@
 package com.GiveaLot.givealot.Events.controller;
 
+import com.GiveaLot.givealot.Events.requests.addCalenderEventRequest;
 import com.GiveaLot.givealot.Events.requests.addTimeLineEventRequest;
 import com.GiveaLot.givealot.Events.response.generalResponse;
 import com.GiveaLot.givealot.Events.service.eventsServiceImp;
@@ -7,6 +8,7 @@ import com.GiveaLot.givealot.Organisation.service.response.responseJSON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,6 +18,20 @@ public class eventsController {
 
     @Autowired
     eventsServiceImp service;
+
+    @PostMapping("/calender/add")
+    ResponseEntity<generalResponse> addCalenderEvent(@RequestBody @NonNull addCalenderEventRequest body)
+    {
+        try
+        {
+            return new ResponseEntity<>(service.addCalenderEvent(body), HttpStatus.OK);
+        }
+        catch (Exception e)
+        {
+            return new ResponseEntity<>(new generalResponse("add_calender_event_bad", "failed"), HttpStatus.BAD_REQUEST);
+        }
+    }
+
 
     @GetMapping("/get/timeline/{orgId}")
     ResponseEntity<responseJSON> getAllEventsForOrganisation(@PathVariable("orgId") Long orgId)
@@ -30,7 +46,7 @@ public class eventsController {
         }
     }
 
-    @GetMapping("/add/timeline/")
+    @PostMapping("/add/timeline/")
     ResponseEntity<generalResponse> addTimelineEvent(@RequestBody addTimeLineEventRequest body)
     {
         try

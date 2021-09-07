dff9ebd4b8d3d99d307e89d70e7e73eb87b6fc30
Xplodiator <33042059+Xplodiator@users.noreply.github.com>
2021-09-07 14:43:52 +0200

    added get certificate as image added get certificate as pdf http requests.

--- backend/src/main/java/com/GiveaLot/givealot/media/Controller/MediaController.java
+++ backend/src/main/java/com/GiveaLot/givealot/media/Controller/MediaController.java
@@ -1,9 +1,9 @@
 package com.GiveaLot.givealot.media.Controller;
 
 import java.io.IOException;
-import javax.servlet.http.HttpServletResponse;
-
+import com.GiveaLot.givealot.media.MediaService.MediaServiceImp;
 import lombok.var;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,36 +13,82 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
 @RestController
 public class MediaController {
 
+    @Autowired
+    MediaServiceImp service;
+
     @RequestMapping(value = "/cert/version/png/{orgId}", method = RequestMethod.GET,
             produces = MediaType.ALL_VALUE)
-    public ResponseEntity<byte[]> getImageCertificate(HttpServletResponse response,@PathVariable("orgId") Long orgId) throws IOException
+    public ResponseEntity<byte[]> getImageCertificate(@PathVariable("orgId") String orgId)
     {
-        var imgFile = new ClassPathResource("localFiles/" +orgId+ "/certificate/CertificateImage.png");
-        byte[] bytes = StreamUtils.copyToByteArray(imgFile.getInputStream());
+        try
+        {
+            if(!service.orgIdExists(Long.valueOf(orgId)))
+            {
+                return ResponseEntity
+                        .notFound().build();
+            }
+        }
+        catch (Exception e)
+        {
+            return ResponseEntity
+                    .notFound().build();
+        }
 
-        return ResponseEntity
-                .ok()
-                .contentType(MediaType.IMAGE_JPEG)
-                .body(bytes);
+        var imgFile = new ClassPathResource("localFiles/" +orgId+ "/certificate/CertificateImage.png");
+
+        byte[] bytes = null;
+        try {
+            bytes = StreamUtils.copyToByteArray(imgFile.getInputStream());
+            return ResponseEntity
+                    .ok()
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .body(bytes);
+        }
+        catch (IOException e)
+        {
+            return ResponseEntity
+                    .notFound().build();
+
+        }
+
+
     }
 
     @RequestMapping(value = "/cert/version/pdf/{orgId}", method = RequestMethod.GET,
             produces = MediaType.APPLICATION_PDF_VALUE)
-    public ResponseEntity<byte[]> getPDFCertificate(HttpServletResponse response,@PathVariable("orgId") Long orgId) throws IOException
+    public ResponseEntity<byte[]> getPDFCertificate(@PathVariable("orgId") String orgId)
     {
-        var imgFile = new ClassPathResource("localFiles/" +orgId+ "/certificate/CertificateComplete.pdf");
-        byte[] bytes = StreamUtils.copyToByteArray(imgFile.getInputStream());
+        try
+        {
+            if(!service.orgIdExists(Long.valueOf(orgId)))
+            {
+                return ResponseEntity
+                        .notFound().build();
+            }
+        }
+        catch (Exception e)
+        {
+            return ResponseEntity
+                    .notFound().build();
+        }
 
-        return ResponseEntity
-                .ok()
-                .contentType(MediaType.APPLICATION_PDF)
-                .body(bytes);
+        var imgFile = new ClassPathResource("localFiles/" +orgId+ "/certificate/CertificateComplete.pdf");
+
+        byte[] bytes = null;
+        try {
+            bytes = StreamUtils.copyToByteArray(imgFile.getInputStream());
+            return ResponseEntity
+                    .ok()
+                    .contentType(MediaType.APPLICATION_PDF)
+                    .body(bytes);
+        }
+        catch (IOException e)
+        {
+            return ResponseEntity
+                 .notFound().build();
+        }
     }
 }

b1df9bea971c6c07b47e5d73b91566509f341f72
TshilidziN <64367951+TshilidziN@users.noreply.github.com>
2021-09-09 00:10:29 +0200

    fetch data for level 0, profile page

--- frontend/givealot/src/views/dashboard/sidebar/Components/Profile/Level0.js
+++ frontend/givealot/src/views/dashboard/sidebar/Components/Profile/Level0.js
@@ -11,6 +11,7 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Axios from "axios";
 import TextField from "@material-ui/core/TextField";
+import axios from "axios";
 
 const styles = theme => ({
 
@@ -34,6 +35,8 @@ const styles = theme => ({
 
 
 const initialState = {
+    level0:[],
+    adminId:14,
     orgId:6,
     startDate: new Date(),
     ngoNumber:"",
@@ -69,9 +72,6 @@ export class Level0 extends Component {
                 : event.target.value
         });
     }
-
-
-
 
 
     handleChange = event => {
@@ -136,19 +136,45 @@ export class Level0 extends Component {
 
 
     };
-    onToast = () => {
-
+    onToast1 = () => {
             toast.success('Submit successful', {
                 position: toast.POSITION.TOP_RIGHT
 
             });
+    }
+
+    componentDidMount() {
+        let config = {
+            headers: {
+                "Content-Type": "application/json",
+                'Access-Control-Allow-Origin': '*',
+            }
+        }
+        const admin = {
+            "adminId" : this.state.adminId
+        }
+
+        const org = {
+            orgId :32
+        }
+        axios.post('http://localhost:8080/v1/organisation/get/organisations',admin , config)
+            .then(response =>{
+                console.log(response)
+                this.setState({level0: response.data.response[0]})
+                console.log(this.state.level0)
+
+            })
+            .catch(error =>{
+                this.setState({error : 'Error Retrieving data'})
+            })
+
 
     }
 
 
     render(){
         const { classes } = this.props;
-
+        const { level0 } = this.state
 
 
         return (
@@ -170,7 +196,7 @@ export class Level0 extends Component {
                                         <DatePicker
 
                                             className="upgrade_datee input3"
-                                            selected={ this.state.startDate }
+                                            selected={this.state.startDate}
                                             onChange={ this.handleDateChange }
                                             name="startDate"
                                             dateFormat="yyyy/MM/dd"
@@ -183,7 +209,7 @@ export class Level0 extends Component {
                                         label="Registration number"
                                         name="ngoNumber"
                                         style={{ margin: 8 }}
-                                        placeholder="Enter registration.."
+                                        placeholder={level0.dateAdded}
                                         fullWidth
                                         margin="normal"
                                         InputLabelProps={{
@@ -206,7 +232,7 @@ export class Level0 extends Component {
 
                                 </div>
                                 <div className="upgrade_Button">
-                                    <button className="upgrade-btnn" type="submit" onClick={this.onToast}>
+                                    <button className="upgrade-btnn" type="submit" onClick={this.onToast1}>
                                         Submit
                                     </button>
                                 </div>

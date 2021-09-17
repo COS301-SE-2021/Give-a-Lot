440671a2a4667c2bec48b9da60f001a7da9ce725
WandaWeira <55826359+WandaWeira@users.noreply.github.com>
2021-09-17 20:19:24 +0200

    conditional rendering side bar

--- frontend/givealot/src/views/dashboard/Dashboard.js
+++ frontend/givealot/src/views/dashboard/Dashboard.js
@@ -25,74 +25,100 @@ import AddOrg from "../dashboard/sidebar/Components/OrganisationsDash/AddOrg"
 import OrgValidate from "../dashboard/sidebar/Components/Validate/OrgValidate"
 import Timeline from "../dashboard/sidebar/Components/Timeline/Timeline"
 
+
+const roles = localStorage.getItem('role')
+
+
 function Dashboard() {
+
+    function features(){
+        if(roles === 'admin')
+        {
+            return(
+                <Switch>
+                    <Route exact path="/dashboard">
+                        <Featured />
+                    </Route>
+                    <Route exact path="/organisations">
+                        <OrganisationsDash />
+                    </Route>
+                    <Route path="/org/:id">
+                        <Org />
+                    </Route>
+                    <Route exact path="/users">
+                        <Users />
+                    </Route>
+                    <Route exact path="/report">
+                        <Reports />
+                    </Route>
+                    <Route exact path="/calendar">
+                        <Calendar />
+                    </Route>
+                    <Route exact path="/validate">
+                        <Validate />
+                    </Route>
+                    <Route exact path="/addOrg">
+                        <AddOrg />
+                    </Route>
+                    <Route exact path="/orgValidate/:id">
+                        <OrgValidate />
+                    </Route>
+                    <Route exact path="/timeline">
+                        <Timeline />
+                    </Route>
+                </Switch>
+            )
+        }
+        else if(roles === 'organisation'){
+            return(
+                <Switch>
+                    <Route exact path="/dashboard">
+                        <Profile />
+                    </Route>
+                    <Route exact path="/report">
+                        <Reports />
+                    </Route>
+                    <Route exact path="/calendar">
+                        <Calendar />
+                    </Route>
+                    <Route exact path="/profile">
+                        <Profile />
+                    </Route>
+                    <Route exact path="/certificate">
+                        <Certificate />
+                    </Route>
+                    <Route exact path="/upgrade1">
+                        <Upgrade1 />
+                    </Route>
+                    <Route exact path="/upgrade2">
+                        <Upgrade2 />
+                    </Route>
+                    <Route exact path="/upgrade3">
+                        <Upgrade3 />
+                    </Route>
+                    <Route exact path="/upgrade4">
+                        <Upgrade4 />
+                    </Route>
+                    <Route exact path="/upgrade5">
+                        <Upgrade5 />
+                    </Route>
+                    <Route exact path="/upgrade0">
+                        <Upgrade0 />
+                    </Route>
+                    <Route exact path="/timeline">
+                        <Timeline />
+                    </Route>
+                </Switch>
+            )
+        }
+    }
+
     return (
         <Router>
             <div className="Dashboard">
                 <div className="DashboardContainer">
                     <Sidebar />
-                    <Switch>
-                        <Route exact path="/dashboard">
-                            <Featured />
-                        </Route>
-                        <Route exact path="/featured">
-                            <Featured />
-                        </Route>
-                        <Route exact path="/organisations">
-                            <OrganisationsDash />
-                        </Route>
-                        <Route path="/org/:id">
-                            <Org />
-                        </Route>
-                        <Route exact path="/users">
-                            <Users />
-                        </Route>
-                        <Route exact path="/email">
-                            <Email />
-                        </Route>
-                        <Route exact path="/report">
-                            <Reports />
-                        </Route>
-                        <Route exact path="/calendar">
-                            <Calendar />
-                        </Route>
-                        <Route exact path="/validate">
-                            <Validate />
-                        </Route>
-                        <Route exact path="/profile">
-                            <Profile />
-                        </Route>
-                        <Route exact path="/certificate">
-                            <Certificate />
-                        </Route>
-                        <Route exact path="/upgrade1">
-                            <Upgrade1 />
-                        </Route>
-                        <Route exact path="/upgrade2">
-                            <Upgrade2 />
-                        </Route>
-                        <Route exact path="/upgrade3">
-                            <Upgrade3 />
-                        </Route>
-                        <Route exact path="/upgrade4">
-                            <Upgrade4 />
-                        </Route>
-                        <Route exact path="/upgrade5">
-                            <Upgrade5 />
-                        </Route>
-                        <Route exact path="/upgrade0">
-                            <Upgrade0 />
-                        </Route>
-                        <Route exact path="/addOrg">
-                            <AddOrg />
-                        </Route>
-                        <Route exact path="/orgValidate/:id">
-                            <OrgValidate />
-                        </Route>
-                        <Route exact path="/timeline">
-                            <Timeline />
-                        </Route>
-                    </Switch>
+                    {features()}
                 </div>
             </div>
         </Router>

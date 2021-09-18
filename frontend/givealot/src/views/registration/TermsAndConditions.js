ed0e56ecb387848ee27699a5374a3f1a179ab3a9
TshilidziN <64367951+TshilidziN@users.noreply.github.com>
2021-09-18 16:52:08 +0200

    Fixing bugs

--- frontend/givealot/src/views/registration/TermsAndConditions.js
+++ frontend/givealot/src/views/registration/TermsAndConditions.js
@@ -34,6 +34,13 @@ function TermsAndConditions()
                             Cras lacinia elit vitae metus eleifend, non ultrices quam vestibulum. Nulla facilisi. Vestibulum fermentum gravida risus, vitae consequat nisi auctor a. Morbi vitae magna volutpat, gravida diam eget, tristique augue. Integer sollicitudin tortor at massa consectetur, vel ultrices risus condimentum. Ut eget interdum ante, at tincidunt ligula. Maecenas aliquam ligula non est maximus, a mattis purus congue. Fusce tortor est, iaculis sit amet tincidunt nec, ultricies quis dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
                         </div>
 
+                        <span className="LoginHeader">
+                           Privacy Statement
+                       </span>
+                        <div className="terms_text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sodales iaculis urna nec interdum. Quisque vel ullamcorper nisi. In blandit dictum mi, a tincidunt ligula mattis non. Pellentesque id mi tristique, posuere risus sit amet, lacinia dui. Cras non bibendum nulla. Suspendisse tempor non urna ac laoreet. Maecenas et dictum dui, dictum sagittis ligula. Curabitur sodales, leo id volutpat euismod, risus purus vulputate ex, sit amet mattis velit mauris vel purus. Phasellus bibendum iaculis quam ut lacinia. Nullam elementum suscipit leo, sed tincidunt mi tincidunt vitae.
+                        </div>
+
                     </div>
                 </div>
             </div>

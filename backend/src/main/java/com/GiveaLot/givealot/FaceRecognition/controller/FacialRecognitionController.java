79b0180e511e89f19a502d58b8f3ff14ebba960f
Kgomotso404 <u19104546@tuks.co.za>
2021-09-20 02:20:14 +0200

    added facial recognition controller

--- backend/src/main/java/com/GiveaLot/givealot/FaceRecognition/controller/FacialRecognitionController.java
+++ backend/src/main/java/com/GiveaLot/givealot/FaceRecognition/controller/FacialRecognitionController.java
@@ -1,10 +1,7 @@
-package com.GiveaLot.givealot.FaceRecognition.service.controller;
+package com.GiveaLot.givealot.FaceRecognition.controller;
 
-import com.GiveaLot.givealot.FaceRecognition.service.FaceRecognitionServiceImpl;
 import com.GiveaLot.givealot.FaceRecognition.service.requests.AddFRImageMultipartRequest;
 import com.GiveaLot.givealot.FaceRecognition.service.response.generalFaceRecognitionResponse;
-import com.GiveaLot.givealot.Organisation.requests.AddOrgImageMultipartRequest;
-import com.GiveaLot.givealot.Organisation.response.generalOrganisationResponse;
 import com.GiveaLot.givealot.Server.ServerAccess;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;

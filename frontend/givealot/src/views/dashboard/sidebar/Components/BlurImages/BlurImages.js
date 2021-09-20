6f9069c309262122b0ebc005a3a582f84703b851
Xplodiator <33042059+Xplodiator@users.noreply.github.com>
2021-09-20 02:48:53 +0200

    backup

--- frontend/givealot/src/views/dashboard/sidebar/Components/BlurImages/BlurImages.js
+++ frontend/givealot/src/views/dashboard/sidebar/Components/BlurImages/BlurImages.js
@@ -8,31 +8,31 @@ function BlurImages()
     let [current_image, set_current_image] = useState(blurredFallback);
     const uploadImage = event =>
     {
-        event.preventDefault();
+        alert("he0")
         const form_data = new FormData();
         form_data.append('orgId', localStorage.getItem('id'));
-        form_data.append('images', event.target.files);
+        form_data.append('images', event.target.files[0]);
 
         console.log(event.target.files)
 
-        set_current_image("http://localhost:8080/logo/version/84")
-
-        /*fetch(
-            'http://localhost:8080/v1/organisation/add/qrcode',
+        fetch(
+            'http://localhost:8080/v1/frecognition/blur',
             {
                 method: 'POST',
                 body: form_data,
             }
         )
         .then((response) => response.json())
-        .then((result) => {
-
+        .then((result) =>
+        {
+            alert("he2")
+            console.log(result)
         })
         .catch((error) => {
             console.error('Error:', error.response);
-        });*/
+            alert("he")
+        });
     }
-
 
     return (
         <div className="blur">

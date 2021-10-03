fc93e00fd20889d1985eba638361ba27d1eec313
Joshua Warneke <u18227369@tuks.co.za>
2021-10-03 17:31:51 +0200

    Added Organisation Data files

---
+++ backend/src/main/java/com/GiveaLot/givealot/Organisation/repository/OrganisationGalleryRepository.java
@@ -0,0 +1,13 @@
+package com.GiveaLot.givealot.Organisation.repository;
+
+import com.GiveaLot.givealot.Organisation.model.OrganisationData;
+import com.GiveaLot.givealot.Organisation.model.OrganisationGallery;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface OrganisationGalleryRepository extends JpaRepository<OrganisationGallery, Long> {
+    @Query("select o from OrganisationGallery o where o.orgId = ?1")
+    OrganisationData selectOrganisationImagesById(Long orgId);
+}

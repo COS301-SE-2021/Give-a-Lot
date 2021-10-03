004d1b79984a7cadea8b22726da775a430e4a3d7
Xplodiator <33042059+Xplodiator@users.noreply.github.com>
2021-10-03 17:27:27 +0200

    gallery images table

---
+++ backend/src/main/java/com/GiveaLot/givealot/Organisation/repository/OrganisationGalleryRepository.java
@@ -0,0 +1,7 @@
+package com.GiveaLot.givealot.Organisation.repository;
+import com.GiveaLot.givealot.Organisation.model.OrganisationGallery;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrganisationGalleryRepository extends JpaRepository<OrganisationGallery, Long> {
+    
+}

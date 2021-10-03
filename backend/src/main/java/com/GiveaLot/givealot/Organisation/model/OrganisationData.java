23fa140c8aaaa1afd9d5bbf8e9fc8c3e7de8b3b2
Xplodiator <33042059+Xplodiator@users.noreply.github.com>
2021-10-03 17:19:37 +0200

    gallery images table

--- backend/src/main/java/com/GiveaLot/givealot/Organisation/model/OrganisationData.java
+++ backend/src/main/java/com/GiveaLot/givealot/Organisation/model/OrganisationData.java
@@ -26,31 +26,6 @@ public class OrganisationData {
     private byte[] qrCode;
 
     @Lob
-    @Column(name="image1")
-    @Type(type="org.hibernate.type.BinaryType")
-    private byte[] image1;
-
-    @Lob
-    @Column(name="image2")
-    @Type(type="org.hibernate.type.BinaryType")
-    private byte[] image2;
-
-    @Lob
-    @Column(name="image3")
-    @Type(type="org.hibernate.type.BinaryType")
-    private byte[] image3;
-
-    @Lob
-    @Column(name="image4")
-    @Type(type="org.hibernate.type.BinaryType")
-    private byte[] image4;
-
-    @Lob
-    @Column(name="image5")
-    @Type(type="org.hibernate.type.BinaryType")
-    private byte[] image5;
-
-    @Lob
     @Column(name="certificate")
     @Type(type="org.hibernate.type.BinaryType")
     private byte[] certificate;
@@ -64,15 +39,10 @@ public class OrganisationData {
 
     }
 
-    public OrganisationData(Long orgId, byte[] auditDoc, byte[] qrCode, byte[] image1, byte[] image2, byte[] image3, byte[] image4, byte[] image5, byte[] certificate, byte[] certificateImage) {
+    public OrganisationData(Long orgId, byte[] auditDoc, byte[] qrCode, byte[] certificate, byte[] certificateImage) {
         this.orgId = orgId;
         this.auditDoc = auditDoc;
         this.qrCode = qrCode;
-        this.image1 = image1;
-        this.image2 = image2;
-        this.image3 = image3;
-        this.image4 = image4;
-        this.image5 = image5;
         this.certificate = certificate;
         this.certificateImage = certificateImage;
     }
